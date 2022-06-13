19e409e1f29b445ad8453dad245996fb78d52f73
Ivo <43663336+ivaaak@users.noreply.github.com>
2022-06-14 00:05:20 +0300

    added areas and scaffolded identity

---
+++ RealEstate/Areas/Admin/Controllers/BaseController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using RealEstate.Core.Constants;
+
+namespace RealEstate.Areas.Admin.Controllers
+{
+    [Authorize(Roles = UserConstants.Roles.Administrator)]
+    [Area("Admin")]
+    public class BaseController : Controller
+    {
+        
+    }
+}
---
+++ RealEstate/Areas/Admin/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RealEstate.Areas.Admin.Controllers
+{
+    public class HomeController : BaseController
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
