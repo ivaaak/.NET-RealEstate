b77d35760b2bdc5968bcc62280fc68d3014a51e9
Ivo <43663336+ivaaak@users.noreply.github.com>
2022-06-19 10:27:33 +0300

    added tests project

---
+++ RealEstate.Test/UnitTest.cs
@@ -0,0 +1,19 @@
+using NUnit.Framework;
+
+namespace RealEstate.Test
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+		  //to do add inMemory DB
+        }
+
+        [Test]
+        public void UnitTest()
+        {
+            Assert.Pass();
+        }
+    }
+}
