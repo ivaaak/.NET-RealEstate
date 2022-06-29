fd3ea7d8ec57f66c0ab7b32916622429e16040df
Ivo <43663336+ivaaak@users.noreply.github.com>
2022-06-29 08:36:16 +0300

    added validators folder + user validation

---
+++ RealEstate.Infrastructure/Validators/CreateUserValidator.cs
@@ -0,0 +1,31 @@
+﻿
+namespace RealEstate.CQRS.Commands.Validators
+{
+    /*
+    public class CreateUserValidator : AbstractValidator<UserViewModel>
+    {
+        public CreateUserValidator()
+        {
+            RuleFor(a => a.Name)
+                .NotEmpty()
+                .WithMessage("Name is required");
+
+            RuleFor(a => a.Email)
+                .EmailAddress()
+                .WithMessage("E-mail isnt valid!");
+
+            RuleFor(a => a.Password)
+                .NotEmpty()
+                .WithMessage("Password is required.");
+
+            RuleFor(a => a.ConfirmPassword)
+                .NotEmpty()
+                .WithMessage("Password confirmation is required");
+
+            RuleFor(a => a.ConfirmPassword)
+                .Equal(b => b.Password)
+                .WithMessage("Passwords dont match.");
+        }
+    }
+    */
+}
