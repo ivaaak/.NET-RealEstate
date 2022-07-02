17cd8aa0c5cf0221f0bfdf977d8941fefc7e8993
Ivo <43663336+ivaaak@users.noreply.github.com>
2022-07-02 12:14:21 +0300

    Added 2 handlers.

---
+++ RealEstate.CQRS/Handlers/DeletePropertyByIdHandler.cs
@@ -0,0 +1,27 @@
+﻿using MediatR;
+using RealEstate.CQRS.Commands;
+using RealEstate.CQRS.Pipeline;
+using RealEstate.Infrastructure.Data;
+using RealEstate.Infrastructure.Data.Repositories;
+
+namespace RealEstate.CQRS.Handlers
+{
+    public class DeletePropertyByIdHandler : IRequestHandler<DeletePropertyByIdCommand, Response>
+    {
+        private readonly IApplicationDbRepository repo;
+
+        public DeletePropertyByIdHandler(IApplicationDbRepository _repo)
+        {
+            repo = _repo;
+        }
+
+        public Task<Response> Handle(DeletePropertyByIdCommand request, CancellationToken cancellationToken)
+        {
+            int id = request.Id;
+            var property = repo.GetByIdAsync<Property>(id);
+            repo.DeleteAsync<Property>(property);
+
+            return (Task<Response>)Task.CompletedTask;
+        }
+    }
+}
