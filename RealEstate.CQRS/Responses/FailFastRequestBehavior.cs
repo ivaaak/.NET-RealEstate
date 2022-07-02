15ec73bc16ff6ba6c8ddbd30e1552a07fe56a9c8
Ivo <43663336+ivaaak@users.noreply.github.com>
2022-07-02 12:15:54 +0300

    Added responses - default and fail-fast.

---
+++ RealEstate.CQRS/Responses/FailFastRequestBehavior.cs
@@ -0,0 +1,44 @@
+﻿using FluentValidation;
+using FluentValidation.Results;
+using MediatR;
+
+namespace RealEstate.CQRS.Responses
+{
+    public class FailFastRequestBehavior<TRequest, TResponse> 
+	  : IPipelineBehavior<TRequest, TResponse>
+        where TRequest : IRequest<TResponse> 
+	  where TResponse : Response
+    {
+        private readonly IEnumerable<IValidator> _validators;
+
+        public FailFastRequestBehavior(IEnumerable<IValidator<TRequest>> validators)
+        {
+            _validators = validators;
+        }
+
+        public Task<TResponse> Handle(TRequest request, CancellationToken cancellationToken, RequestHandlerDelegate<TResponse> next)
+        {
+            var failures = _validators
+                .Select(v => v.Validate((IValidationContext)request))
+                .SelectMany(result => result.Errors)
+                .Where(f => f != null)
+                .ToList();
+
+            return failures.Any()
+                ? Errors(failures)
+                : next();
+        }
+
+        private static Task<TResponse> Errors(IEnumerable<ValidationFailure> failures)
+        {
+            var response = new Response();
+
+            foreach (var failure in failures)
+            {
+                response.AddError(failure.ErrorMessage);
+            }
+
+            return Task.FromResult(response as TResponse);
+        }
+    }
+}
---
+++ RealEstate.CQRS/Responses/Response.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.ObjectModel;
+
+namespace RealEstate.CQRS.Responses
+{
+    public class Response
+    {
+        private readonly IList<string> _messages = new List<string>();
+
+        public IEnumerable<string> Errors { get; }
+        public object Result { get; }
+
+        public Response() => Errors = new ReadOnlyCollection<string>(_messages);
+
+        public Response(object result) : this() => Result = result;
+
+        public Response AddError(string message)
+        {
+            _messages.Add(message);
+            return this;
+        }
+    }
+}
