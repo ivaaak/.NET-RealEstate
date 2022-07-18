4e9fa7941235c927a410729a78bef1dd2c83e759
ivaaak <ivaaakpavlov@gmail.com>
2022-07-18 23:33:07 +0300

    Moved validators to Core Project

---
+++ RealEstate.Core/Validators/LoginValidator.cs
@@ -0,0 +1,33 @@
+﻿using FluentValidation;
+using RealEstate.Core.ViewModels;
+
+namespace RealEstate.Core.Validators
+{
+
+    public class LoginValidator : AbstractValidator<LoginViewModel>
+    {
+        public LoginValidator()
+        {
+            RuleFor(a => a.Name)
+                .NotEmpty()
+                .WithMessage("Name is required");
+
+            RuleFor(a => a.Email)
+                .EmailAddress()
+                .WithMessage("E-mail isnt valid!");
+
+            RuleFor(a => a.Password)
+                .NotEmpty()
+                .WithMessage("Password is required.");
+
+            RuleFor(a => a.ConfirmPassword)
+                .NotEmpty()
+                .WithMessage("Password confirmation is required");
+
+            RuleFor(a => a.ConfirmPassword)
+                .Equal(b => b.Password)
+                .WithMessage("Passwords dont match.");
+        }
+    }
+
+}
