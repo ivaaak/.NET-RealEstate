0db3c1c10a711d20d993602a7be369c8e3373030
Ivaylo Pavlov <43663336+ivaaak@users.noreply.github.com>
2022-11-17 14:16:13 +0200

    Refactoring

---
+++ RealEstate.CQRS/Models/RequestModels/Request.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.ObjectModel;
+
+namespace RealEstate.CQRS.RequestModels
+{
+    public class Request
+    {
+        //This is a placeholder and to be redone
+        private readonly IList<string> data = new List<string>();
+
+        public IEnumerable<string> Data { get; }    
+    }
+}
