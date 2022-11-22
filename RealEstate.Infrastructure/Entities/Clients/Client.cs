3545261a6763e2d9277bd4b873fbf785d252321b
Ivaylo Pavlov <ivaaakpavlov@gmail.com>
2022-11-22 19:48:15 +0200

    feat: Reworking entities - clients

---
+++ RealEstate.Infrastructure/Entities/Clients/Client.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace RealEstate.Infrastructure.Entities.Clients
+{
+    public class Client : IdentityUser
+    {
+        //public override Guid Id { get; init; }
+
+        public string Client_Name { get; init; }
+
+        public string Client_Address { get; init; }
+
+        public string Contact_Person { get; init; }
+
+        public string Phone { get; init; }
+
+        public string Mobile { get; init; }
+
+        public string EMail { get; init; }
+
+        public string Client_Details { get; init; }
+
+        public DateTime Time_Created { get; init; }
+    }
+}
---
+++ RealEstate.Infrastructure/Entities/Clients/Contact.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RealEstate.Infrastructure.Entities.Clients
+{
+    public class Contact
+    {
+        [Key]
+        public int Id { get; init; }
+
+        public int Client_Id { get; init; }
+
+        public int Employee_Id { get; init; }
+
+        public int Estate_Id { get; init; }
+
+        public DateTime Contact_Time { get; init; }
+
+        public string Contact_Details { get; init; }
+    }
+}
