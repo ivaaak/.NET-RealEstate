220828babe0a5840f6c662ac609f0796177c4c85
Ivaylo Pavlov <ivaaakpavlov@gmail.com>
2022-11-22 19:48:46 +0200

    feat: Reworking entities - listings

---
+++ RealEstate.Infrastructure/Entities/Listings/Employee.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Identity;
+using RealEstate.Infrastructure.Entities.Estates;
+
+namespace RealEstate.Infrastructure.Entities.Listings
+{
+    public class Employee : IdentityUser
+    {
+        public string First_Name { get; init; }
+
+        public string Last_Name { get; init; }
+
+        public IEnumerable<Listing> Listings { get; set; }
+
+        public IEnumerable<Estate> Estates { get; init; }
+
+        public Employee() : base()
+        {
+            Listings = new List<Listing>();
+
+            Estates = new List<Estate>();
+        }
+    }
+}
---
+++ RealEstate.Infrastructure/Entities/Listings/Listing.cs
@@ -0,0 +1,28 @@
+﻿using RealEstate.Infrastructure.Entities.Estates;
+using System.ComponentModel.DataAnnotations;
+
+namespace RealEstate.Infrastructure.Entities.Listings
+{
+    public class Listing
+    {
+        [Key]
+        public int Id { get; init; }
+
+        public string Name { get; set; }
+
+        public Estate Estate { get; set; }
+
+        public int Price { get; set; }
+
+        public double PricePerSquareMeter
+        {
+            set
+            {
+                if (value > 0 && Estate != null)
+                {
+                    //value = Price / Property.SquareMeters;
+                }
+            }
+        }
+    }
+}
