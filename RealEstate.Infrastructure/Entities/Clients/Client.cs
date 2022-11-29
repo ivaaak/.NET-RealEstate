53f2c7ae493a24bb0df4c39f2b206cbf1791814c
Ivaylo Pavlov <ivaaakpavlov@gmail.com>
2022-11-29 20:22:23 +0200

    fix: client entity properties initialised with a setter

--- RealEstate.Infrastructure/Entities/Clients/Client.cs
+++ RealEstate.Infrastructure/Entities/Clients/Client.cs
@@ -9,24 +9,24 @@ namespace RealEstate.Infrastructure.Entities.Clients
         [ForeignKey("Client_Id")]
         public string Client_Id { get; init; }
 
-        public string Client_Name { get; init; }
+        public string Client_Name { get; set; }
 
-        public string Client_Address { get; init; }
+        public string Client_Address { get; set; }
 
-        public string Contact_Person { get; init; }
+        public string Contact_Person { get; set; }
 
-        public string Phone { get; init; }
+        public string Phone { get; set; }
 
-        public string Mobile { get; init; }
+        public string Mobile { get; set; }
 
-        public string EMail { get; init; }
+        public string EMail { get; set; }
 
-        public string Client_Details { get; init; }
+        public string Client_Details { get; set; }
 
         public DateTime Time_Created { get; init; }
 
-        public Contact Contact { get; init; }
+        public Contact Contact { get; set; }
 
-        public IEnumerable<Contract> Contracts { get; init; }
+        public IEnumerable<Contract> Contracts { get; set; }
     }
 }
