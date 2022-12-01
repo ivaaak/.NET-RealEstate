438c6421211a18e1ec52338e6c5bf17bee73602c
Ivaylo Pavlov <ivaaakpavlov@gmail.com>
2022-12-01 20:14:00 +0200

    feat: added setup for testing - inMemoryDB, test constants

---
+++ RealEstate.Test/Constants/TestConstants.cs
@@ -0,0 +1,33 @@
+﻿using RealEstate.Infrastructure.Entities.Clients;
+using RealEstate.Infrastructure.Entities.Contracts;
+
+namespace RealEstate.Test.Constants
+{
+    public class TestConstants
+    {
+        public static Client client = new Client()
+        {
+            Id = "1",
+            Client_Id = "1",
+            Client_Address = "ul. Hubavka",
+            Client_Details = "details placeholder",
+            Client_Name = "Giga Chikadze",
+            Email = "",
+            Mobile = "088",
+            Phone = "02",
+        };
+
+        public static Contact contact = new Contact() { };
+
+        public static Contract contract = new Contract() { };
+
+        public static Contract_Invoice contract_invoice = new Contract_Invoice() { };
+
+        public static Contract_Type contract_type = new Contract_Type() { };
+
+        public static Payment_Frequency payment_frequency = new Payment_Frequency() { };
+
+        public static Under_Contract under_contract = new Under_Contract() { };
+
+    }
+}
---
+++ RealEstate.Test/Data/InMemoryDbContext.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
+using RealEstate.Infrastructure.Context;
+
+namespace RealEstate.Test.Data
+{
+    public class InMemoryDbContext : DbContext
+    {
+        private readonly SqliteConnection connection;
+        private readonly DbContextOptions<ApplicationDbContext> dbContextOptions;
+
+        public InMemoryDbContext()
+        {
+            connection = new SqliteConnection("Filename=:memory:");
+            connection.Open();
+
+            dbContextOptions = new DbContextOptionsBuilder<ApplicationDbContext>()
+                .UseSqlite(connection)
+                .Options;
+
+            using var context = new ApplicationDbContext(dbContextOptions);
+
+            context.Database.EnsureCreated();
+
+        }
+
+        public ApplicationDbContext CreateContext() => new ApplicationDbContext(dbContextOptions);
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder
+                    .UseSqlite(connection);
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //TODO add entity relations
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
