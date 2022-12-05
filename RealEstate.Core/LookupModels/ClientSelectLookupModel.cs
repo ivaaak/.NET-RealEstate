8a9ed40994850de3fccddd4256c06603d95b0eb9
Ivaylo Pavlov <ivaaakpavlov@gmail.com>
2022-12-05 20:14:17 +0200

    feat: custom lookup models + mapping

---
+++ RealEstate.Core/LookupModels/ClientSelectLookupModel.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using RealEstate.Core.Mapping;
+using RealEstate.Infrastructure.Entities.Clients;
+
+namespace RealEstate.Core.LookupModels
+{
+    public class ClientSelectLookupModel : IMapFrom<Client>, IHasCustomMapping
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public void CreateMappings(Profile configuration)
+        {
+            configuration.CreateMap<Client, ClientSelectLookupModel>();
+        }
+    }
+}
