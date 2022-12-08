9bc0a1e40de8f7c3d257c4d2ea0dab65527f065e
Ivaylo Pavlov <ivaaakpavlov@gmail.com>
2022-12-08 18:30:47 +0200

    feat: db interfaces - query runner and seeder

---
+++ RealEstate.Infrastructure/Interfaces/IDatabaseSeeder.cs
@@ -0,0 +1,7 @@
+﻿namespace RealEstate.Infrastructure.Interfaces
+{
+    public abstract class IDatabaseSeeder
+    {
+        public partial Task SeedAsync(IApplicationDbContext databaseContext, IServiceProvider serviceProvider);
+    }
+}
