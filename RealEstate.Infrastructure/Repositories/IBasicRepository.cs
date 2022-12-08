12c848cf7e73efb9fcac79ab80a8729980288e65
Ivaylo Pavlov <ivaaakpavlov@gmail.com>
2022-12-08 18:29:29 +0200

    feat: added more repo types - basic and deletable entity

---
+++ RealEstate.Infrastructure/Repositories/IBasicRepository.cs
@@ -0,0 +1,18 @@
+﻿namespace RealEstate.Infrastructure.Repositories
+{
+    public interface IRepository<TEntity> : IDisposable
+        where TEntity : class
+    {
+        IQueryable<TEntity> All();
+
+        IQueryable<TEntity> AllAsNoTracking();
+
+        Task AddAsync(TEntity entity);
+
+        void Update(TEntity entity);
+
+        void Delete(TEntity entity);
+
+        Task<int> SaveChangesAsync();
+    }
+}
---
+++ RealEstate.Infrastructure/Repositories/IDeletableEntityRepository.cs
@@ -0,0 +1,18 @@
+﻿using RealEstate.Infrastructure.Entities.BaseEntityModel;
+
+namespace RealEstate.Infrastructure.Repositories
+{
+    public interface IDeletableEntityRepository<TEntity> : IRepository<TEntity>
+        where TEntity : class, IDeletableEntity
+    {
+        IQueryable<TEntity> AllWithDeleted();
+
+        IQueryable<TEntity> AllAsNoTrackingWithDeleted();
+
+        Task<TEntity> GetByIdWithDeletedAsync(params object[] id);
+
+        void HardDelete(TEntity entity);
+
+        void Undelete(TEntity entity);
+    }
+}
