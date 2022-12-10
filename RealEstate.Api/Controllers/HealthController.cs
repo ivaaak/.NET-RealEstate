65409490d59caa2fce02ca286f645bc092764fca
Ivaylo Pavlov <ivaaakpavlov@gmail.com>
2022-12-10 12:31:18 +0200

    feat: added health controller

---
+++ RealEstate.Api/Controllers/HealthController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace RealEstate.API.Controllers
+{
+    [Route("health")]
+    [ApiController]
+    [AllowAnonymous]
+    public class HealthController : ControllerBase
+    {
+        [HttpGet, Route("")]
+        public String Alive() => "alive";
+
+#if DEBUG
+        [HttpGet, Route("test")]
+        public String Test()
+        {
+            return "test";
+        }
+#endif
+    }
+}
