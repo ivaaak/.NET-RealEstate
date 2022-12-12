4d53f95015d2c80996e4b6908d5422c6b9201d6d
Ivaylo Pavlov <ivaaakpavlov@gmail.com>
2022-12-12 18:31:28 +0200

    feat: query fixes

--- RealEstate.CQRS/Handlers/Query/GetClientByIdHandler.cs
+++ RealEstate.CQRS/Handlers/Query/GetClientByIdHandler.cs
@@ -9,10 +9,17 @@ namespace RealEstate.CQRS.Handlers.Query
     public class GetClientByIdHandler : IRequestHandler<GetClientByIdQuery, ClientViewModel>
     {
         private readonly IApplicationDbRepository repo;
+        
+        public string Id { get; set; }
 
         public GetClientByIdHandler(IApplicationDbRepository _repo)
         {
             repo = _repo;
+        }
+
+        public GetClientByIdHandler(string id)
+        {
+            Id = id;
         }
 
         public  Task<ClientViewModel> Handle(GetClientByIdQuery request, CancellationToken cancellationToken)
--- RealEstate.CQRS/Queries/EstatesSearchQuery.cs
+++ RealEstate.CQRS/Queries/EstatesSearchQuery.cs
@@ -6,5 +6,12 @@ namespace RealEstate.CQRS.Queries
     public class EstatesSearchQuery : IRequest<SearchViewModel>
     {
         public string? Query { get; set; }
+
+        public string? City { get; set; }
+        public int? MinPrice { get; set; }
+        public int? MaxPrice { get; set; }
+        public string? Id { get; set; }
+        public string? Name { get; set; }
+        public string? Sort { get; set; }
     }
 }
