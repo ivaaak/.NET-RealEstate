edad31599dddd27a2dfd95ebf32727436207d2ac
Ivaylo Pavlov <ivaaakpavlov@gmail.com>
2022-12-13 19:48:30 +0200

    feat: moved db contexts to data project

---
+++ RealEstate.Data/Context/ListingsDBContext.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using RealEstate.Data.Identity;
+using RealEstate.Models.Entities.Estates;
+using RealEstate.Models.Entities.Listings;
+
+namespace RealEstate.Data.Context
+{
+    public class ListingsDBContext : IdentityDbContext<ApplicationUser>
+    {
+        public ListingsDBContext(DbContextOptions<ListingsDBContext> options)
+            : base(options) { }
+
+        public DbSet<Employee> Employees { get; set; }
+
+        public DbSet<Listing> Listings { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Employee>().HasIndex(c => c.Id).IsUnique();
+            modelBuilder.Entity<Listing>().HasIndex(li => li.Id).IsUnique();
+
+            modelBuilder
+                .Entity<Employee>()
+                .HasOne(cl => cl.Company)
+                .WithMany(cl => cl.Employees)
+                .HasForeignKey(cl => cl.Id)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            modelBuilder
+               .Entity<Listing>()
+               .HasOne(li => li.Estate)
+               .WithOne(es => es.Listing)
+               .HasForeignKey<Estate>(es => es.Listing)
+               .OnDelete(DeleteBehavior.Restrict);
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
