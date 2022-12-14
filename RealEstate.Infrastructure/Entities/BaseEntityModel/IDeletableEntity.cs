36dd93211d1ffc4da05641e693fa461157c8d57a
Ivaylo Pavlov <ivaaakpavlov@gmail.com>
2022-12-14 19:12:58 +0200

    feat: removed entities from Infrastructure project

--- RealEstate.Infrastructure/Entities/BaseEntityModel/IDeletableEntity.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace RealEstate.Infrastructure.Entities.BaseEntityModel
-{
-    public interface IDeletableEntity
-    {
-        bool IsDeleted { get; set; }
-
-        DateTime? DeletedOn { get; set; }
-    }
-}
