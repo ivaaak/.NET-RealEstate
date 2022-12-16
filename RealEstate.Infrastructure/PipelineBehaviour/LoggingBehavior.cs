b0e44d2169aa9ef3d3eca8a37b550aa69ac16658
Ivaylo Pavlov <ivaaakpavlov@gmail.com>
2022-12-16 17:40:07 +0200

    feat: added logging pipeline behavior

---
+++ RealEstate.Infrastructure/PipelineBehaviour/LoggingBehavior.cs
@@ -0,0 +1,25 @@
+﻿using MediatR;
+using Microsoft.Extensions.Logging;
+
+namespace RealEstate.Infrastructure.PipelineBehaviour
+{
+    public class LoggingBehavior<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse>
+    where TRequest : IRequest<TResponse>
+    {
+        private readonly ILogger<LoggingBehavior<TRequest, TResponse>> _logger;
+
+        public LoggingBehavior(ILogger<LoggingBehavior<TRequest, TResponse>> logger)
+        {
+            _logger = logger;
+        }
+
+        public async Task<TResponse> Handle(TRequest request, CancellationToken cancellationToken, RequestHandlerDelegate<TResponse> next)
+        {
+            _logger.LogInformation($"Handling {typeof(TRequest).Name}");
+            var response = await next();
+            _logger.LogInformation($"Handled {typeof(TResponse).Name}");
+
+            return response;
+        }
+    }
+}
