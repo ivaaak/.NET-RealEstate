f223d06d018e6947f60019ba30b54a43c2d86d34
Ivaylo Pavlov <ivaaakpavlov@gmail.com>
2022-12-16 17:39:55 +0200

    feat: added auth pipeline behavior

---
+++ RealEstate.Infrastructure/PipelineBehaviour/AuthenticationBehavior.cs
@@ -0,0 +1,61 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Http;
+
+namespace RealEstate.Infrastructure.Authentication
+{
+    public class AuthenticationBehavior<TRequest, TResponse> 
+        : IPipelineBehavior<TRequest, TResponse> 
+        where TRequest : IRequest<TResponse>
+    {
+        private readonly IHttpContextAccessor _httpContextAccessor;
+
+        public AuthenticationBehavior(IHttpContextAccessor httpContextAccessor)
+        {
+            _httpContextAccessor = httpContextAccessor;
+        }
+
+        public async Task<TResponse> Handle(TRequest request,
+            CancellationToken cancellationToken,
+            RequestHandlerDelegate<TResponse> next)
+        {
+            var httpContext = _httpContextAccessor.HttpContext;
+
+            if (!httpContext.User.Identity.IsAuthenticated)
+            {
+                throw new UnauthorizedAccessException();
+            }
+
+            return await next();
+        }
+
+
+        public async Task<object> Handle(IRequest request,
+        CancellationToken cancellationToken,
+        RequestHandlerDelegate<object> next)
+        {
+            var user = _httpContextAccessor.HttpContext.User;
+
+            if (!user.Identity.IsAuthenticated || 
+                !user.IsInRole("admin") || 
+                !IsAuthorized(request))
+            {
+                throw new UnauthorizedAccessException();
+            }
+
+            return await next();
+        }
+
+        private bool IsAuthorized(IRequest request)
+        {
+            var context = _httpContextAccessor.HttpContext;
+
+            var user = context.User;
+
+            if(!user.Identity.IsAuthenticated || !user.IsInRole("admin"))
+            {
+                return false;
+            }
+            return true;
+        }
+    }
+}
