053e4a87efddef8d8fc9ec8b616379290e19aea9
Ivaylo Pavlov <ivaaakpavlov@gmail.com>
2022-12-18 11:03:47 +0200

    feat: added data models - auth0user, file, emailRequest

---
+++ RealEstate.Models/Entities/Misc/SendEmailRequest.cs
@@ -0,0 +1,13 @@
+﻿namespace RealEstate.Models.Entities.Misc
+{
+    public class SendEmailRequest
+    {
+        public string From { get; set; }
+
+        public string To { get; set; }
+
+        public string Subject { get; set; }
+
+        public string HtmlContent { get; set; }
+    }
+}
