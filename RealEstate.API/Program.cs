1638b2aacd9609dc46cfc8e634d0533d00e97c8d
Ivaylo Pavlov <ivaaakpavlov@gmail.com>
2022-12-22 18:10:24 +0200

    feat: added API features - xml docs, swagger, swaggerUI

--- RealEstate.API/Program.cs
+++ RealEstate.API/Program.cs
@@ -1,5 +1,7 @@
 using Hangfire;
 using MediatR;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Mvc.ApiExplorer;
 using RealEstate.API.ServiceExtensions;
 using RealEstate.CQRS;
 
@@ -15,7 +17,7 @@ builder.Services.AddJWTAuthentication();
 builder.Services.AddHangfireWithPostgreSQLServer(builder.Configuration);
 builder.Services.AddModelBinders();
 builder.Services.AddApplicationServices();
-builder.Services.AddSwaggerAPIWithEndpoints();
+builder.Services.AddApiVersioningConfigured();
 builder.Services.AddMediatR(typeof(MediatREntryPoint).Assembly); //Reference to the CQRS Assembly
 
 var app = builder.Build();
@@ -28,9 +30,24 @@ app.UseHangfireDashboard();
 
 app.MapControllerRoute(name: "Area", pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}");
 app.MapControllerRoute(name: "default",pattern: "{controller=Home}/{action=Index}/{id?}");
-
-//app.MapRazorPages();
 app.MapControllers();
+
+if (app.Environment.IsDevelopment())
+{
+    // Enable middleware to serve the generated OpenAPI definition as JSON files.
+    app.UseSwagger();
+
+    // Enable middleware to serve Swagger-UI (HTML, JS, CSS, etc.) by specifying the Swagger JSON endpoint(s).
+    var descriptionProvider = app.Services.GetRequiredService<IApiVersionDescriptionProvider>();
+    app.UseSwaggerUI(options =>
+    {
+        // Build a swagger endpoint for each discovered API version
+        foreach (var description in descriptionProvider.ApiVersionDescriptions)
+        {
+            options.SwaggerEndpoint($"{description.GroupName}/swagger.json", description.GroupName.ToUpperInvariant());
+        }
+    });
+}
 
 //var developmentFlag = app.Environment.IsDevelopment() ? app.UseMigrationsEndPoint() : app.UseExceptionHandler("/Home/Error");
 
