799d7b9a9539f535afd36de19a3b3251a460bbfb
Ivaylo Pavlov <ivaaakpavlov@gmail.com>
2022-12-24 10:49:20 +0200

    feat: added crawler project/ testing how to crawl a site/link

---
+++ RealEstate.Crawler/ImotBGCrawler.cs
@@ -0,0 +1,108 @@
+﻿using HtmlAgilityPack;
+using Npgsql;
+using System.Net;
+
+namespace RealEstate.Crawler
+{
+    public class ImotBGCrawler
+    {
+        private string html;
+
+        private 
+
+        public void RunCrawler()
+        {
+            this.DownloadWebsiteString();
+            this.ExtractWebsiteData();
+        }
+
+
+        public void DownloadWebsiteString()
+        {
+            using (var client = new WebClient())
+            {
+                html = client.DownloadString("https://www.imot.bg/pcgi/imot.cgi?act=3&slink=8qwd4b&f1=1");
+            }
+        }
+
+
+        public void ExtractWebsiteData()
+        {
+            var htmlDoc = new HtmlDocument();
+            htmlDoc.LoadHtml(html);
+
+            var propertyNameNode = htmlDoc.DocumentNode.SelectSingleNode("//h1[@class='headerClass']");
+            var propertyName = propertyNameNode.InnerText.Trim();
+
+            var priceNode = htmlDoc.DocumentNode.SelectSingleNode("//div[@class='priceClass']");
+            var price = priceNode.InnerText.Trim();
+
+            var descriptionNode = htmlDoc.DocumentNode.SelectSingleNode("//div[@class='descriptionClass']");
+            var description = descriptionNode.InnerText.Trim();
+        }
+
+        public void Do()
+        {
+            var htmlDoc = new HtmlDocument();
+            htmlDoc.LoadHtml(html);
+
+            var listings = htmlDoc.DocumentNode.SelectNodes("//div[@class='offer-item']"); //fix?
+
+            foreach (var listing in listings)
+            {
+                // Extract data from each listing
+                var propertyNameNode = listing.SelectSingleNode(".//h3[@class='title']");
+                var propertyName = propertyNameNode.InnerText.Trim();
+
+                var priceNode = listing.SelectSingleNode(".//p[@class='price']");
+                var price = priceNode.InnerText.Trim();
+
+                var descriptionNode = listing.SelectSingleNode(".//p[@class='description']");
+                var description = descriptionNode.InnerText.Trim();
+            }
+        }
+
+        public void SaveDataToDatabase()
+        {
+            var htmlDoc = new HtmlDocument();
+            htmlDoc.LoadHtml(html);
+
+            var nodes = htmlDoc.DocumentNode.SelectNodes("//div[@class='offer-item']");
+
+            using (var writer = new StreamWriter("data.csv"))
+            {
+                // Write the column headers
+                writer.WriteLine("Property Name,Price,Description");
+
+                // Iterate through the nodes and extract the data
+                foreach (var node in nodes)
+                {
+                    var propertyName = node.SelectSingleNode(".//h3[@class='title']").InnerText.Trim();
+                    var price = node.SelectSingleNode(".//p[@class='price']").InnerText.Trim();
+                    var description = node.SelectSingleNode(".//p[@class='description']").InnerText.Trim();
+
+                    // Write the data to the CSV file
+                    writer.WriteLine($"{propertyName},{price},{description}");
+                }
+            }
+
+            using (var conn = new NpgsqlConnection("Host=localhost;Username=user;Password=password;Database=database"))
+            {
+                conn.Open();
+
+                foreach (var node in nodes)
+                {
+                    var propertyName = node.SelectSingleNode(".//h3[@class='title']").InnerText.Trim();
+                    var price = node.SelectSingleNode(".//p[@class='price']").InnerText.Trim();
+                    var description = node.SelectSingleNode(".//p[@class='description']").InnerText.Trim();
+
+                    var command = new NpgsqlCommand("INSERT INTO properties (name, price, description) VALUES (@name, @price, @description)", conn);
+                    command.Parameters.AddWithValue("@name", propertyName);
+                    command.Parameters.AddWithValue("@price", price);
+                    command.Parameters.AddWithValue("@description", description);
+                    command.ExecuteNonQuery();
+                }
+            }
+        }
+    }
+}
