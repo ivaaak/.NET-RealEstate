31f29fbabf12d6f8487493b42f4601337ec0a4ce
Ivaylo Pavlov <ivaaakpavlov@gmail.com>
2022-12-27 11:08:16 +0200

    feat: added dependency factory, autofac skeleton.

---
+++ RealEstate.API/DependencyFactory.cs
@@ -0,0 +1,68 @@
+﻿using Algolia.Search.Clients;
+using RealEstate.Core.Guards;
+using RealEstate.Microservices.Auth0;
+using RealEstate.Microservices.Cache;
+using RealEstate.Microservices.Email;
+using RealEstate.Microservices.Estates;
+using RealEstate.Microservices.FileUpload;
+using RealEstate.Microservices.Listings;
+using RealEstate.Microservices.Serializer;
+using RealEstate.Microservices.Sorting;
+using RealEstate.Microservices.Users;
+
+namespace RealEstate.API
+{
+    internal sealed class DependencyFactory : IDependencyFactory
+    {
+        private Dictionary<Type, object> dependencyMap;
+
+        public DependencyFactory(
+            IAccountClient accountClient,
+            IAuth0AuthenticationService auth0AuthenticationService,
+            ICacheService cacheService,
+            IEmailService emailService,
+            IEstateService estateService,
+            IFileUploadService fileUploadService,
+            IListingService listingService,
+            IJsonSerializer jsonSerializer,
+            ISortingService sortingService,
+            IUserService userService)
+        {
+            Validate.NotNull(accountClient, nameof(accountClient));
+            Validate.NotNull(auth0AuthenticationService, nameof(auth0AuthenticationService));
+            Validate.NotNull(cacheService, nameof(cacheService));
+            Validate.NotNull(emailService, nameof(emailService));
+            Validate.NotNull(estateService, nameof(estateService));
+            Validate.NotNull(fileUploadService, nameof(fileUploadService));
+            Validate.NotNull(listingService, nameof(listingService));
+            Validate.NotNull(jsonSerializer, nameof(jsonSerializer));
+            Validate.NotNull(sortingService, nameof(sortingService));
+            Validate.NotNull(userService, nameof(userService));
+
+            dependencyMap = new Dictionary<Type, object>();
+
+            dependencyMap[typeof(IAccountClient)] = accountClient;
+            dependencyMap[typeof(IAuth0AuthenticationService)] = auth0AuthenticationService;
+            dependencyMap[typeof(ICacheService)] = cacheService;
+            dependencyMap[typeof(IEmailService)] = emailService;
+            dependencyMap[typeof(IEstateService)] = estateService;
+            dependencyMap[typeof(IFileUploadService)] = fileUploadService;
+            dependencyMap[typeof(IListingService)] = listingService;
+            dependencyMap[typeof(IJsonSerializer)] = jsonSerializer;
+            dependencyMap[typeof(ISortingService)] = sortingService;
+            dependencyMap[typeof(IUserService)] = userService;
+        }
+
+        public T Get<T>()
+        {
+            Validate.ContainsKey(dependencyMap, typeof(T), nameof(dependencyMap), typeof(T).FullName);
+
+            return (T)dependencyMap[typeof(T)];
+        }
+    }
+
+    public interface IDependencyFactory
+    {
+        T Get<T>();
+    }
+}
---
+++ RealEstate.API/ServiceExtensions/AutofacExtension.cs
@@ -0,0 +1,44 @@
+﻿using Autofac;
+using RealEstate.Core.Configurations;
+
+namespace RealEstate.API.ServiceExtensions
+{
+    public class AutofacExtension
+    {
+        public void ConfigureContainer(ContainerBuilder builder)
+        {
+            EnvironmentDefinition environment = EnvironmentDefinition.GetEnvironmentDefinition();
+
+            // initializes classes that provides static interfaces
+            //KeyVaultClient.Init(environment.KeyVaultConfiguration);
+
+            // start async registrations
+            Task configureTasks = ConfigureContainerAsync(environment);
+
+            // Type Registrations
+            builder.RegisterType<DependencyFactory>()
+                .As<IDependencyFactory>()
+                .SingleInstance();
+
+            // Module registrations
+            //builder.RegisterModule(new CloudQueueClientModule());
+
+
+            // wait for async registrations to complete
+            configureTasks.Wait();
+        }
+
+        private async Task ConfigureContainerAsync(EnvironmentDefinition environment)
+        {
+            // start tasks
+            // Task<string> getJwtSecretsTask = KeyVaultClient.GetSecretAsync(SecretNames.JwtSecrets);
+
+            // wait for completion
+            // string jwtSecretsString = await getJwtSecretsTask;
+            // var jwtSecrets = jwtSecretsString.Deserialize<JwtSecrets>();
+
+            // configure
+            // JwtToken.SetJwtSecrets(jwtSecrets);
+        }
+    }
+}
