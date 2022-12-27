5ae3b7639356daa34d0705b353315dd679793c1d
Ivaylo Pavlov <ivaaakpavlov@gmail.com>
2022-12-27 11:09:06 +0200

    feat: http exception classes

---
+++ RealEstate.Core/Exceptions/HttpExceptionBase.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Net;
+
+namespace RealEstate.Core.Exceptions
+{
+    public class HttpExceptionBase : Exception
+    {
+        public HttpStatusCode StatusCode { get; set; }
+        public object Content { get; set; }
+
+        public HttpExceptionBase(HttpStatusCode statusCode, string message = null, Exception innerException = null)
+            : base(message, innerException)
+        {
+            StatusCode = statusCode;
+        }
+    }
+}
