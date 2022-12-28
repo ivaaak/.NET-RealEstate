fe6a9f2008aeef0c72a7db7ec544e2c9db5311cd
Ivaylo Pavlov <ivaaakpavlov@gmail.com>
2022-12-28 12:14:52 +0200

    feat: classes to make working with dates easier

---
+++ RealEstate.Core/Date.cs
@@ -0,0 +1,44 @@
+﻿using RealEstate.Core.Constants;
+using System.ComponentModel;
+
+namespace RealEstate.Core
+{
+    [TypeConverter(typeof(DateTypeConverter))]
+    public sealed class Date : IEquatable<Date>, IComparable<Date>
+    {
+        private readonly DateTime dateTime;
+
+        public Date(DateTime dateTime)
+        {
+            this.dateTime = dateTime;
+        }
+
+        public Date(string dateTime)
+        {
+            if (DateTime.TryParse(dateTime, out DateTime date) == false)
+            {
+                throw new ArgumentException($"Invalid {nameof(dateTime)} {dateTime}");
+            }
+
+            this.dateTime = date;
+        }
+
+        public Date AddDays(int days)
+        {
+            DateTime dateTime = this.dateTime.AddDays(days);
+            return new Date(dateTime);
+        }
+
+        public DateTime ToDateTime() => dateTime;
+
+        public override string ToString() => dateTime.ToString(DateFormats.DateYmd);
+
+        public bool Equals(Date other)
+            => other != null && ToString() == other.ToString();
+
+        public int CompareTo(Date other)
+        {
+            return dateTime.CompareTo(other.dateTime);
+        }
+    }
+}
---
+++ RealEstate.Core/DateTimeExtension.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RealEstate.Core
+{
+    public static class DateTimeExtensions
+    {
+        public static Date ToDate(this DateTime dateTime)
+        {
+            return new Date(dateTime);
+        }
+    }
+}
