5aff9660eaf65d0008954eea30697df0224311fe
Ivaylo Pavlov <ivaaakpavlov@gmail.com>
2022-12-29 12:02:08 +0200

    feat: extensions - jtoken and object-result

---
+++ RealEstate.Core/IEnumerableExtension.cs
@@ -0,0 +1,15 @@
+﻿namespace RealEstate.Core
+{
+    public static class IEnumerableExtension
+    {
+        public static bool IsNullOrEmpty<T>(this string value)
+        {
+            return string.IsNullOrEmpty(value);
+        }
+
+        public static bool IsNullOrEmpty<T>(this IEnumerable<T> value)
+        {
+            return value == null || value.Any() == false;
+        }
+    }
+}
