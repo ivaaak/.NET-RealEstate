4772fd91605af978914a1f1ac3cbe74f3f3d3da4
Ivaylo Pavlov <ivaaakpavlov@gmail.com>
2023-01-01 12:33:01 +0200

    refactor: chained calls on startup

--- RealEstate.API/Program.cs
+++ RealEstate.API/Program.cs
@@ -6,31 +6,33 @@ using RealEstate.CQRS;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddServicesFactory();
-builder.Services.Use_PostgreSQL_Context(builder.Configuration);
-//builder.Services.Use_MicrosoftSQL_Context(builder.Configuration);
-builder.Services.AddAuth0Authentication(builder.Configuration);
-builder.Services.AddAutoMapper();
-builder.Services.AddIdentityContext();
-builder.Services.AddJWTAuthentication();
-//builder.Services.AddHangfire();
-builder.Services.AddHangfireWithPostgreSQLServer(builder.Configuration);
-builder.Services.AddModelBinders();
-builder.Services.AddApiVersioningConfigured();
-builder.Services.AddMediatR(typeof(MediatREntryPoint).Assembly); //Reference to the CQRS Assembly
-
+builder.Services
+    .AddServicesFactory()
+    .Use_PostgreSQL_Context(builder.Configuration)
+    .AddAuth0Authentication(builder.Configuration)
+    .AddAutoMapper()
+    .AddIdentityContext()
+    .AddJWTAuthentication()
+    .AddHangfireWithPostgreSQLServer(builder.Configuration)
+    .AddModelBinders()
+    .AddApiVersioningConfigured()
+    .AddMediatR(typeof(MediatREntryPoint).Assembly); //Reference to the CQRS Assembly
+    //.Use_MicrosoftSQL_Context(builder.Configuration);
+    //.AddHangfire();
 
 var app = builder.Build();
 
 // App routing
-app.UseHttpsRedirection().UseStaticFiles();
-app.UseRouting();
-app.UseAuthentication().UseAuthorization();
-app.UseHangfireDashboard();
+app.UseHttpsRedirection()
+    .UseStaticFiles()
+    .UseRouting()
+    .UseAuthentication()
+    .UseAuthorization()
+    .UseHangfireDashboard();
 
+app.MapControllers();
 app.MapControllerRoute(name: "Area", pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}");
 app.MapControllerRoute(name: "default",pattern: "{controller=Home}/{action=Index}/{id?}");
-app.MapControllers();
 
 if (app.Environment.IsDevelopment())
 {
