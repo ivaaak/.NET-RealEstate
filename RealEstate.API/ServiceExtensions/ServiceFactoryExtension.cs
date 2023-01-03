05d7ebb9c9d1887d72e69002e4fefd382faf6792
Ivaylo Pavlov <ivaaakpavlov@gmail.com>
2023-01-03 10:27:15 +0200

    refactor: split up service collection into db connections and service factory

---
+++ RealEstate.API/ServiceExtensions/ServiceFactoryExtension.cs
@@ -0,0 +1,35 @@
+﻿using RealEstate.Data.Repository;
+using RealEstate.Microservices.Auth0;
+using RealEstate.Microservices.Cache;
+using RealEstate.Microservices.Email;
+using RealEstate.Microservices.Estates;
+using RealEstate.Microservices.FileUpload;
+using RealEstate.Microservices.Listings;
+using RealEstate.Microservices.Notification;
+using RealEstate.Microservices.Serializer;
+using RealEstate.Microservices.Sorting;
+using RealEstate.Microservices.Users;
+
+namespace RealEstate.API.ServiceExtensions
+{
+    public static class ServiceFactoryExtension
+    {
+        public static IServiceCollection AddServicesFactory(this IServiceCollection services)
+        {
+            services.AddScoped<IApplicationDbRepository, ApplicationDbRepository>();
+            services.AddScoped<IUserService, UserService>();
+            services.AddTransient<IEmailService, EmailService>();
+            services.AddTransient<IAuth0AuthenticationService, Auth0AuthenticationService>();
+            services.AddTransient<ICacheService, CacheService>();
+            services.AddTransient<IEstateService, EstateService>();
+            services.AddTransient<IFileUploadService, FileUploadService>();
+            services.AddTransient<IListingService, ListingService>();
+            services.AddTransient<INotificationService, NotificationService>();
+            services.AddTransient<IJsonSerializer, JsonSerializer>();
+            services.AddTransient<ISortingService, EstateSortingService>();
+
+
+            return services;
+        }
+    }
+}
