51cc9c038060f8d6fbca58d1271d9f7ddaf76759
Ivaylo Pavlov <ivaaakpavlov@gmail.com>
2023-01-17 18:16:15 +0200

    feat: deployment DTO

---
+++ RealEstate.Orchestration/DTO/Deployment.cs
@@ -0,0 +1,11 @@
+﻿namespace RealEstate.Orchestration.DTO
+{
+    public class Deployment
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Source { get; set; }
+
+        public override string ToString() => $"Deployment [Id={Id}, Name={Name}]";
+    }
+}
