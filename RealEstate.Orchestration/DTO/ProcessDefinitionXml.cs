8ba3f1db7c04e111cf4394fd55a65a675dfcbe00
Ivaylo Pavlov <ivaaakpavlov@gmail.com>
2023-01-18 10:36:45 +0200

    feat: process definition dtos

---
+++ RealEstate.Orchestration/DTO/ProcessDefinitionXml.cs
@@ -0,0 +1,11 @@
+﻿namespace RealEstate.Orchestration.DTO
+{
+    public class ProcessDefinitionXml
+    {
+        public string Id { get; set; }
+        public string Bpmn20Xml { get; set; }
+
+        public override string ToString() => $"ProcessDefinitionXml [Id={Id}]";
+    }
+
+}
