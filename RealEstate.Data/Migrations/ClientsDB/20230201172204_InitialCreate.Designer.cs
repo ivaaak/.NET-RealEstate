abe4303e9c2f0c05d81d0bd6b13ab335f180f2fe
Ivaylo Pavlov <ivaaakpavlov@gmail.com>
2023-02-02 18:29:25 +0200

    feat: updated clients db migration

--- RealEstate.Data/Migrations/ClientsDB/20230201172204_InitialCreate.Designer.cs
+++ RealEstate.Data/Migrations/ClientsDB/20230201172204_InitialCreate.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using RealEstate.Data.Context;
@@ -11,9 +12,11 @@ using RealEstate.Data.Context;
 namespace RealEstate.Data.Migrations.ClientsDB
 {
     [DbContext(typeof(ClientsDBContext))]
-    partial class ClientsDBContextModelSnapshot : ModelSnapshot
+    [Migration("20230201172204_InitialCreate")]
+    partial class InitialCreate
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
