61377939421e84226ba4008fa3e0699d7c390062
Ivaylo Pavlov <ivaaakpavlov@gmail.com>
2023-02-03 19:34:46 +0200

    feat: contracts db migration update

--- RealEstate.Data/Migrations/ContractsDB/20230201172231_TestMigration.Designer.cs
+++ RealEstate.Data/Migrations/ContractsDB/20230201172231_TestMigration.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using RealEstate.Data.Context;
@@ -11,9 +12,11 @@ using RealEstate.Data.Context;
 namespace RealEstate.Data.Migrations.ContractsDB
 {
     [DbContext(typeof(ContractsDBContext))]
-    partial class ContractsDBContextModelSnapshot : ModelSnapshot
+    [Migration("20230201172231_TestMigration")]
+    partial class TestMigration
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
