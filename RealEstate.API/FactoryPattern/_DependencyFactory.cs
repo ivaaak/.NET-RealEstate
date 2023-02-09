0d8ad52b7db26e9eb47cde3eb451015753fb04f4
Ivaylo Pavlov <ivaaakpavlov@gmail.com>
2023-02-09 18:28:34 +0200

    refactor: commented out unused/ not imported services

--- RealEstate.API/FactoryPattern/_DependencyFactory.cs
+++ RealEstate.API/FactoryPattern/_DependencyFactory.cs
@@ -1,13 +1,5 @@
-﻿using Algolia.Search.Clients;
-using RealEstate.API.Authentication.Contracts;
+﻿using RealEstate.API.Authentication.Contracts;
 using RealEstate.Core.Guards;
-using RealEstate.Microservices.Email;
-using RealEstate.Microservices.Estates;
-using RealEstate.Microservices.Listings;
-using RealEstate.Microservices.Sorting;
-using RealEstate.Microservices.Utils.Cache;
-using RealEstate.Microservices.Utils.FileUpload;
-using RealEstate.Microservices.Utils.Serializer;
 
 namespace RealEstate.API.FactoryPattern
 {
@@ -16,40 +8,40 @@ namespace RealEstate.API.FactoryPattern
         private Dictionary<Type, object> dependencyMap;
 
         public _DependencyFactory(
-            IAccountClient accountClient,
-            IAuth0Service auth0AuthenticationService,
-            ICacheService cacheService,
-            IEmailService emailService,
-            IEstateService estateService,
-            IFileUploadService fileUploadService,
-            IListingService listingService,
-            IJsonSerializer jsonSerializer,
-            ISortingService sortingService,
-            IUserService userService)
+             IUserService userService,
+            IAuth0Service auth0AuthenticationService)
+            //IAccountClient accountClient,
+            //ICacheService cacheService,
+            //IEmailService emailService,
+            //IEstateService estateService,
+            //IFileUploadService fileUploadService,
+            //IListingService listingService,
+            //IJsonSerializer jsonSerializer,
+            //ISortingService sortingService)
         {
-            Validate.NotNull(accountClient, nameof(accountClient));
             Validate.NotNull(auth0AuthenticationService, nameof(auth0AuthenticationService));
-            Validate.NotNull(cacheService, nameof(cacheService));
-            Validate.NotNull(emailService, nameof(emailService));
-            Validate.NotNull(estateService, nameof(estateService));
-            Validate.NotNull(fileUploadService, nameof(fileUploadService));
-            Validate.NotNull(listingService, nameof(listingService));
-            Validate.NotNull(jsonSerializer, nameof(jsonSerializer));
-            Validate.NotNull(sortingService, nameof(sortingService));
             Validate.NotNull(userService, nameof(userService));
+            //Validate.NotNull(accountClient, nameof(accountClient));
+            //Validate.NotNull(cacheService, nameof(cacheService));
+            //Validate.NotNull(emailService, nameof(emailService));
+            //Validate.NotNull(estateService, nameof(estateService));
+            //Validate.NotNull(fileUploadService, nameof(fileUploadService));
+            //Validate.NotNull(listingService, nameof(listingService));
+            //Validate.NotNull(jsonSerializer, nameof(jsonSerializer));
+            //Validate.NotNull(sortingService, nameof(sortingService));
 
             dependencyMap = new Dictionary<Type, object>();
 
-            dependencyMap[typeof(IAccountClient)] = accountClient;
             dependencyMap[typeof(IAuth0Service)] = auth0AuthenticationService;
-            dependencyMap[typeof(ICacheService)] = cacheService;
-            dependencyMap[typeof(IEmailService)] = emailService;
-            dependencyMap[typeof(IEstateService)] = estateService;
-            dependencyMap[typeof(IFileUploadService)] = fileUploadService;
-            dependencyMap[typeof(IListingService)] = listingService;
-            dependencyMap[typeof(IJsonSerializer)] = jsonSerializer;
-            dependencyMap[typeof(ISortingService)] = sortingService;
             dependencyMap[typeof(IUserService)] = userService;
+            //dependencyMap[typeof(IAccountClient)] = accountClient;
+            //dependencyMap[typeof(ICacheService)] = cacheService;
+            //dependencyMap[typeof(IEmailService)] = emailService;
+            //dependencyMap[typeof(IEstateService)] = estateService;
+            //dependencyMap[typeof(IFileUploadService)] = fileUploadService;
+            //dependencyMap[typeof(IListingService)] = listingService;
+            //dependencyMap[typeof(IJsonSerializer)] = jsonSerializer;
+            //dependencyMap[typeof(ISortingService)] = sortingService;
         }
 
         public T Get<T>()
--- RealEstate.API/ServiceExtensions/ServiceFactoryExtension.cs
+++ RealEstate.API/ServiceExtensions/ServiceFactoryExtension.cs
@@ -1,14 +1,6 @@
 ﻿using RealEstate.API.Authentication;
 using RealEstate.API.Authentication.Contracts;
 using RealEstate.Data.Repository;
-using RealEstate.Microservices.Email;
-using RealEstate.Microservices.Estates;
-using RealEstate.Microservices.Listings;
-using RealEstate.Microservices.Notification;
-using RealEstate.Microservices.Sorting;
-using RealEstate.Microservices.Utils.Cache;
-using RealEstate.Microservices.Utils.FileUpload;
-using RealEstate.Microservices.Utils.Serializer;
 
 namespace RealEstate.API.ServiceExtensions
 {
@@ -18,16 +10,15 @@ namespace RealEstate.API.ServiceExtensions
         {
             services.AddScoped<IApplicationDbRepository, ApplicationDbRepository>();
             services.AddScoped<IUserService, UserService>();
-            services.AddTransient<IEmailService, EmailService>();
+            //services.AddTransient<IEmailService, EmailService>();
             services.AddTransient<IAuth0Service, Auth0Service>();
-            services.AddTransient<ICacheService, CacheService>();
-            services.AddTransient<IEstateService, EstateService>();
-            services.AddTransient<IFileUploadService, FileUploadService>();
-            services.AddTransient<IListingService, ListingService>();
-            services.AddTransient<INotificationService, NotificationService>();
-            services.AddTransient<IJsonSerializer, JsonSerializer>();
-            services.AddTransient<ISortingService, EstateSortingService>();
-
+            //services.AddTransient<ICacheService, CacheService>();
+            //services.AddTransient<IEstateService, EstateService>();
+            //services.AddTransient<IFileUploadService, FileUploadService>();
+            //services.AddTransient<IListingService, ListingService>();
+            //services.AddTransient<INotificationService, NotificationService>();
+            //services.AddTransient<IJsonSerializer, JsonSerializer>();
+            //services.AddTransient<ISortingService, EstateSortingService>();
 
             return services;
         }
