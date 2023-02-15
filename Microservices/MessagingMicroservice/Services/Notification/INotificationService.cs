aaa2aec5ac7749a7ad6f0ad4ab2d7896a1c58aa4
Ivaylo Pavlov <ivaaakpavlov@gmail.com>
2023-02-15 18:47:47 +0200

    feat: messaging ms - email notifications

---
+++ Microservices/MessagingMicroservice/Services/Notification/INotificationService.cs
@@ -0,0 +1,11 @@
+﻿namespace MessagingMicroservice.Services.Notification
+{
+    public interface INotificationService
+    {
+        // SEND NOTIFICATION
+        void SendNotification(string email, string message);
+
+        // SCHEDULE NOTIFICATION
+        void ScheduleNotification(string email, string message, DateTime time);
+    }
+}
