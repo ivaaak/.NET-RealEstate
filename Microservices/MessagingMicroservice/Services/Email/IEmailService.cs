4844acc93f099400c9c1c25cb0e4666c79b05263
Ivaylo Pavlov <ivaaakpavlov@gmail.com>
2023-02-15 18:47:11 +0200

    feat: messaging ms - email service

---
+++ Microservices/MessagingMicroservice/Services/Email/IEmailService.cs
@@ -0,0 +1,14 @@
+﻿namespace MessagingMicroservice.Services.Email
+{
+    public interface IEmailService
+    {
+        // SEND EMAIL
+        Task SendEmailAsync(string from, string to, string subject, string htmlContent);
+
+        // SEND EMAIL WITH ATTACHMENT
+        Task SendEmailWithAttachmentAsync(string from, string to, string subject, string htmlContent, string fileName, Stream fileStream);
+
+        // SEND EMAIL WITH CUSTOM HEADER
+        Task SendEmailWithCustomHeadersAsync(string from, string to, string subject, string htmlContent, Dictionary<string, string> headers);
+    }
+}
