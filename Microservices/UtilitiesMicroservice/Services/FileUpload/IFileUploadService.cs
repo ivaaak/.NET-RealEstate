5f6b9cb30f83f89f53c88e690d1d8b2e566d5366
Ivaylo Pavlov <ivaaakpavlov@gmail.com>
2023-02-18 13:19:07 +0200

    feat: utilities - file service

---
+++ Microservices/UtilitiesMicroservice/Services/FileUpload/IFileUploadService.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace UtilitiesMicroservice.Services.FileUpload
+{
+    public interface IFileUploadService
+    {
+        // UPLOAD
+        Task<bool> UploadFile(IFormFile file, string fileType);
+
+        // HARD DELETE
+        Task<bool> DeleteFile(int id);
+
+        // SOFT DELETE
+        Task<bool> SoftDeleteFile(int id);
+
+        // UPDATE FILE
+        Task<bool> UpdateFile(int id, IFormFile file, string fileType);
+    }
+}
