e58075586d0d0bb4974e87abbf393959c5dad721
Ivaylo Pavlov <ivaaakpavlov@gmail.com>
2023-02-25 15:02:58 +0200

    refactor: moved extensions

---
+++ RealEstate.Core/Extensions/DateTimeExtension.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using RealEstate.Core.Types;
+
+namespace RealEstate.Core.Extensions
+{
+    public static class DateTimeExtensions
+    {
+        public static Date ToDate(this DateTime dateTime)
+        {
+            return new Date(dateTime);
+        }
+    }
+}
