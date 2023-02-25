79244baefb68416e460b2b3a06cf9c3184102e63
Ivaylo Pavlov <ivaaakpavlov@gmail.com>
2023-02-25 15:04:18 +0200

    feat: data interfaces - context, redis, user acessor

---
+++ RealEstate.Data/Interfaces/IDatabaseQueryRunner.cs
@@ -0,0 +1,7 @@
+﻿namespace RealEstate.Infrastructure.Interfaces
+{
+    public interface IDatabaseQueryRunner : IDisposable
+    {
+        Task RunQueryAsync(string query, params object[] parameters);
+    }
+}
---
+++ RealEstate.Data/Interfaces/IRedisService.cs
@@ -0,0 +1,11 @@
+﻿namespace RealEstate.Infrastructure.Interfaces
+{
+    public interface IRedisService<T>
+    {
+        Task<T> Get(string key);
+
+        Task Save(string key, T obj, TimeSpan? expiration = null);
+
+        Task Delete(string key);
+    }
+}
