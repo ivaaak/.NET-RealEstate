1e7b3bcd7abc7dc8581296b61530f4006bae40d5
Ivaylo Pavlov <ivaaakpavlov@gmail.com>
2023-03-12 11:10:10 +0200

    fix: contracts ms startup - port, appsettings

--- Microservices/ContractsMicroservice/Program.cs
+++ Microservices/ContractsMicroservice/Program.cs
@@ -8,6 +8,8 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Configuration.AddJsonFile("Properties/appsettings.json");
+
 var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
