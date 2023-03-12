5d479b255e65fdd0b2fa59a85cdc077f92a34bc6
Ivaylo Pavlov <ivaaakpavlov@gmail.com>
2023-03-12 11:09:45 +0200

    fix: clients ms startup - port, appsettings

--- Microservices/ClientsMicroservice/Program.cs
+++ Microservices/ClientsMicroservice/Program.cs
@@ -7,6 +7,8 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Configuration.AddJsonFile("Properties/appsettings.json");
+
 var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
