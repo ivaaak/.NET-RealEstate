bf6b96f90706a75781fd672a32d3253140498729
Ivaylo Pavlov <ivaaakpavlov@gmail.com>
2023-03-13 18:29:54 +0200

    fix: launch settings/ports/config

--- Microservices/ExternalAPIsMicroservice/Program.cs
+++ Microservices/ExternalAPIsMicroservice/Program.cs
@@ -5,7 +5,6 @@ var builder = WebApplication.CreateBuilder(args);
 // Run on port 9004
 builder.WebHost.UseUrls("http://*:9004");
 
-// Add services to the container.
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerWithConfig();
@@ -14,7 +13,6 @@ builder.Configuration.AddJsonFile("Properties/appsettings.json");
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger().UseSwaggerUI();
