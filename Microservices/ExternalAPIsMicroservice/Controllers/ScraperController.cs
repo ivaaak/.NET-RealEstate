ecdb14724bee757ec79a55d1d6fa9ad8214383bc
Ivaylo Pavlov <ivaaakpavlov@gmail.com>
2023-03-21 18:37:01 +0200

    feat: controller for the scraper ms

---
+++ Microservices/ExternalAPIsMicroservice/Controllers/ScraperController.cs
@@ -0,0 +1,37 @@
+﻿using ExternalAPIsMicroservice.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using RealEstate.Shared.Models.Entities.Misc;
+
+namespace ExternalAPIsMicroservice.Controllers
+{
+    [Authorize]
+    [ApiController]
+    [Consumes("application/json")]
+    [Produces("application/json")]
+    [Route("api/[controller]")] // api/payments/
+    public class ScraperController
+    {
+        private readonly IScraperService scraper;
+
+        public ScraperController(IScraperService scraper) 
+        {
+            this.scraper = scraper;
+        }
+
+        [HttpPost]
+        [Route("runScraper")]
+        public string RunScraperAndSaveToDB([FromBody] ChargeDataModel model)
+        {
+            try
+            {
+                scraper.RunScraper();
+                return "Completed!";
+            }
+            catch (Exception ex)
+            {
+                return "Error while running scraper: ";
+            }
+        }
+    }
+}
