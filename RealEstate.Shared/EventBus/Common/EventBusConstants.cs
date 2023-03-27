9dc1ebcce9709e0df240cfb3c3d2825320821090
Ivaylo Pavlov <ivaaakpavlov@gmail.com>
2023-03-27 19:53:11 +0300

    feat: event bus constants, base event, test event

---
+++ RealEstate.Shared/EventBus/Common/EventBusConstants.cs
@@ -0,0 +1,13 @@
+﻿namespace RealEstate.Shared.EventBus.Common
+{
+    public static class EventBusConstants
+    {
+        public const string EstatesQueue = "estates-queue";
+
+        public const string ListingsQueue = "listings-queue";
+
+        public const string MessageQueue = "message-queue";
+
+        public const string NotificationsQueue = "notifications-queue";
+    }
+}
---
+++ RealEstate.Shared/EventBus/Events/IntegrationBaseEvent.cs
@@ -0,0 +1,21 @@
+﻿namespace RealEstate.Shared.EventBus.Events
+{
+    public class IntegrationBaseEvent
+    {
+        public IntegrationBaseEvent()
+        {
+            Id = Guid.NewGuid();
+            CreationDate = DateTime.UtcNow;
+        }
+
+        public IntegrationBaseEvent(Guid id, DateTime createDate)
+        {
+            Id = id;
+            CreationDate = createDate;
+        }
+
+        public Guid Id { get; private set; }
+
+        public DateTime CreationDate { get; private set; }
+    }
+}
