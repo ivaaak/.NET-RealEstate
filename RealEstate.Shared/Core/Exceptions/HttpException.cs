ebe8b255475a51a6c2a7722d8b1b9800c38ba521
Ivaylo Pavlov <ivaaakpavlov@gmail.com>
2023-03-29 19:17:18 +0300

    refactor: no longer using base controller

--- RealEstate.Shared/Core/Exceptions/HttpException.cs
+++ RealEstate.Shared/Core/Exceptions/HttpException.cs
@@ -1,11 +1,10 @@
-﻿using System;
-using System.Net;
+﻿using System.Net;
 
 namespace RealEstate.Shared.Core.Exceptions
 {
     public class HttpException : HttpExceptionBase
     {
-        public HttpException(HttpStatusCode statusCode, string message, Exception innerException = null)
+        public HttpException(HttpStatusCode statusCode, string message, Exception? innerException = null)
             : base(statusCode, message, innerException)
         {
         }
