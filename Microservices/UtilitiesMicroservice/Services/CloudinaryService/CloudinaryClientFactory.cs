d54821412843adfa406b76a29953fe7ac0fc125a
Ivaylo Pavlov <ivaaakpavlov@gmail.com>
2023-04-10 21:01:49 +0300

    feat: cloudinary instance factory to ease use

---
+++ Microservices/UtilitiesMicroservice/Services/CloudinaryService/CloudinaryClientFactory.cs
@@ -0,0 +1,22 @@
+﻿using CloudinaryDotNet;
+
+namespace UtilitiesMicroservice.Services.CloudinaryService
+{
+    public class CloudinaryClientFactory
+    {
+        public static Cloudinary GetInstance(IConfiguration configuration)
+        {
+            var cloud = configuration["cloudinary-cloud"];
+
+            var apiKey = configuration["cloudinary-apiKey"];
+
+            var apiSecret = configuration["cloudinary-apiSecret"];
+
+            Account account = new Account(cloud, apiKey, apiSecret);
+
+            Cloudinary clientInstance = new Cloudinary(account);
+
+            return clientInstance;
+        }
+    }
+}
