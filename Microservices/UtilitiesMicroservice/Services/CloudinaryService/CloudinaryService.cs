06c0561691f091e1c48ba59f2b9e0ffccecade8a
Ivaylo Pavlov <ivaaakpavlov@gmail.com>
2023-04-10 21:01:11 +0300

    feat: cloudinary upload service for image files

---
+++ Microservices/UtilitiesMicroservice/Services/CloudinaryService/CloudinaryService.cs
@@ -0,0 +1,45 @@
+﻿using CloudinaryDotNet;
+using CloudinaryDotNet.Actions;
+
+namespace UtilitiesMicroservice.Services.CloudinaryService
+{
+    public class CloudinaryService : ICloudinaryService
+    {
+        private readonly Cloudinary _cloudinaryClient;
+
+        public CloudinaryService(Cloudinary cloudinaryClient)
+        {
+            this._cloudinaryClient = cloudinaryClient ?? throw new ArgumentNullException(nameof(cloudinaryClient));
+        }
+
+        public async Task<string> UploadImage(
+            IFormFile formFileObject,
+            string name,
+            Transformation? transformation = null)
+        {
+            formFileObject = formFileObject ?? throw new ArgumentNullException(nameof(formFileObject));
+
+            byte[] imageFile;
+
+            using (var memoryStream = new MemoryStream())
+            {
+                await formFileObject.CopyToAsync(memoryStream);
+                imageFile = memoryStream.ToArray();
+            }
+
+            var imageStream = new MemoryStream(imageFile);
+
+            var uploadParameters = new ImageUploadParams()
+            {
+                File = new FileDescription(name, imageStream),
+                Transformation = transformation
+            };
+
+            var uploadResult = _cloudinaryClient.Upload(uploadParameters);
+
+            imageStream.Dispose();
+
+            return uploadResult.SecureUri.AbsoluteUri;
+        }
+    }
+}
---
+++ Microservices/UtilitiesMicroservice/Services/CloudinaryService/ICloudinaryService.cs
@@ -0,0 +1,12 @@
+﻿using CloudinaryDotNet;
+
+namespace UtilitiesMicroservice.Services.CloudinaryService
+{
+    public interface ICloudinaryService
+    {
+        Task<string> UploadImage(
+            IFormFile fileForm,
+            string name,
+            Transformation? transformation = null);
+    }
+}
