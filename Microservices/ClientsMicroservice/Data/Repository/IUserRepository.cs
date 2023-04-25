bdf5740dc67b2d44cc95b3b114f2e7a4e10a30dc
Ivaylo Pavlov <ivaaakpavlov@gmail.com>
2023-04-25 19:04:08 +0300

    feat: keycloak settings, export, user repo

---
+++ Microservices/ClientsMicroservice/Data/Repository/IUserRepository.cs
@@ -0,0 +1,23 @@
+﻿using RealEstate.Shared.Models.Entities.Users;
+using System.Linq.Expressions;
+
+namespace ClientsMicroservice.Data.Repository
+{
+    public interface IUserRepository
+    {
+        Task<IEnumerable<UserEntity>> FindAsync(int pageNumber = 0, int pageSize = 10);
+
+        Task<IEnumerable<UserEntity>> FindAsync(
+            Expression<Func<UserEntity, bool>> query,
+            int pageNumber = 0,
+            int pageSize = 10);
+
+        Task<UserEntity> FindByUsernameAsync(string name);
+
+        Task<UserEntity> FindByIdAsync(Guid id);
+
+        Task<IEnumerable<UserAttribute>> GetAttributesAsync(Guid userId);
+
+        Task UpdateAsync(UserEntity user);
+    }
+}
---
+++ Microservices/ClientsMicroservice/Data/Repository/UserRepository.cs
@@ -0,0 +1,76 @@
+﻿#nullable disable
+using Microsoft.EntityFrameworkCore;
+using RealEstate.Shared.Models.Entities.Users;
+using System.Linq.Expressions;
+
+namespace ClientsMicroservice.Data.Repository
+{
+    public class UserRepository : IUserRepository
+    {
+        private readonly UserContext _context;
+
+        public UserRepository(UserContext context)
+        {
+            _context = context;
+        }
+
+
+        public async Task<IEnumerable<UserEntity>> FindAsync(int pageNumber = 0, int pageSize = 10)
+        {
+            return await _context.UserEntities.Skip(pageNumber * pageSize).Take(pageSize).ToListAsync();
+        }
+
+
+        public async Task<IEnumerable<UserEntity>> FindAsync(Expression<Func<UserEntity, bool>> query, int pageNumber = 0,
+            int pageSize = 10)
+        {
+            return await _context.UserEntities.Where(query).Skip(pageNumber * pageSize).Take(pageSize).ToListAsync();
+        }
+
+
+        public Task UpdateAsync(UserEntity userEntity)
+        {
+            _context.UserEntities.Update(userEntity);
+            return _context.SaveChangesAsync();
+        }
+
+        public async Task<UserEntity> FindByIdAsync(Guid id)
+        {
+            return await _context.UserEntities.FirstAsync(x => x.Id == id.ToString());
+        }
+
+
+        public async Task<UserEntity> FindByUsernameAsync(string username)
+        {
+            return await _context.UserEntities.FirstOrDefaultAsync(x => x.Username == username);
+        }
+
+
+        public async Task<IEnumerable<UserAttribute>> GetAttributesAsync(Guid userId)
+        {
+            var user = await _context.UserEntities.Include(x => x.UserAttributes).FirstAsync(x => x.Id == userId.ToString());
+            if (user == null)
+            {
+                return null;
+            }
+            return user.UserAttributes;
+        }
+
+
+        //public async Task<KeycloakUserEntity> InsertOneAsync(KeycloakUserEntity userEntity)
+        //{
+        //    // save userEntity
+        //    _context.UserEntities.Add(userEntity);
+        //    await _context.SaveChangesAsync();
+
+        //    return userEntity;
+        //}
+
+
+        //public Task DeleteAsync(KeycloakUserEntity userEntity)
+        //{
+        //    _context.UserEntities.Remove(userEntity);
+        //    return _context.SaveChangesAsync();
+        //}
+    }
+}
