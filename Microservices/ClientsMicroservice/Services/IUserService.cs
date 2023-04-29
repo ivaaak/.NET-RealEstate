8454ea0a91c08f1d195f85f73b73915d64cfa9fb
Ivaylo Pavlov <ivaaakpavlov@gmail.com>
2023-04-29 15:06:22 +0300

    feat: UserService and interface - user repo access methods

---
+++ Microservices/ClientsMicroservice/Services/IUserService.cs
@@ -0,0 +1,22 @@
+﻿using RealEstate.Shared.Models.Entities.Users;
+
+namespace ClientsMicroservice.Services
+{
+    public interface IUserService
+    {
+        // GetUserById
+        public Task<UserEntity> GetUserById(Guid UserId);
+
+
+        // GetUserByUserName
+        public Task<UserEntity> GetUserByUserName(string Username);
+
+
+        // GetUserAttributesById
+        public Task<IEnumerable<UserAttribute>> GetUserAttributesById(Guid UserId);
+
+
+        // GetUsers
+        public IEnumerable<UserEntity> GetUsers(int Page, int PageSize);
+    }
+}
---
+++ Microservices/ClientsMicroservice/Services/UserService.cs
@@ -0,0 +1,61 @@
+﻿using AutoMapper;
+using ClientsMicroservice.Data.Repository;
+using RealEstate.Shared.Models.Entities.Users;
+
+namespace ClientsMicroservice.Services
+{
+    public class UserService : IUserService
+    {
+        private readonly IUserRepository _userRepository;
+
+        private readonly IMapper _mapper;
+
+        public UserService(IUserRepository userRepository, IMapper mapper)
+        {
+            _userRepository = userRepository;
+            _mapper = mapper;
+        }
+
+
+        // GetUserById
+        public Task<UserEntity> GetUserById(Guid UserId)
+        {
+            var user = _userRepository.FindByIdAsync(UserId);
+
+            return user;
+        }
+
+        // GetUserByUserName
+        public Task<UserEntity> GetUserByUserName(string Username)
+        {
+            var user = _userRepository.FindByUsernameAsync(Username);
+
+            return user;
+        }
+
+        // GetUserAttributesById
+        public Task<IEnumerable<UserAttribute>> GetUserAttributesById(Guid UserId)
+        {
+            var attributes = _userRepository.GetAttributesAsync(UserId);
+
+            return attributes;
+        }
+
+
+        // GetUsers
+        public IEnumerable<UserEntity> GetUsers(int Page, int PageSize)
+        {
+            IEnumerable<UserEntity> result;
+            if (PageSize != 0)
+            {
+                result = _userRepository.FindAsync(Page, PageSize).Result;
+            }
+            else
+            {
+                result = _userRepository.FindAsync().Result;
+            }
+
+            return result;
+        }
+    }
+}
