c481602e9d8e0f334914e9a6f604fd70a9ef68a0
Ivaylo Pavlov <ivaaakpavlov@gmail.com>
2023-04-29 15:07:44 +0300

    feat: user controller with get endpoints

---
+++ Microservices/ClientsMicroservice/Controllers/UserController.cs
@@ -0,0 +1,80 @@
+﻿using ClientsMicroservice.Data;
+using ClientsMicroservice.Services;
+using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using RealEstate.Shared.Models.DTOs.Users;
+using Swashbuckle.AspNetCore.Annotations;
+using System.ComponentModel.DataAnnotations;
+
+namespace ClientsMicroservice.Controllers
+{
+    [ApiController]
+    [Route("api/v1/[controller]/[action]")]
+    [Authorize]
+    public class UserController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+        private readonly IUserService _userService;
+
+        public UserController(IMediator mediator, IUserService userService)
+        {
+            _mediator = mediator;
+            _userService = userService;
+        }
+
+
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(UserDto))]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [Authorize]
+        [SwaggerOperation("This method can be used by an authorized user.")]
+        public async Task<IActionResult> GetCurrentlyLoggedInUser()
+        {
+            var userId = User.GetLoggedInUserId();
+
+            //var response = await _mediator.GetQueryNotNull<UserDto>(new GetUserByIdQuery() { Id = userId });
+            var response = await _userService.GetUserById(userId);
+
+            return Ok(response);
+        }
+
+        [HttpGet("{userId}")]
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(UserDto))]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [Authorize(Roles = "Admin")]
+        [SwaggerOperation("This method can only be used by the administrator.")]
+        public async Task<IActionResult> GetUserById([Required] Guid userId)
+        {
+            //var response = await _mediator.GetQueryNotNull<UserDto>(new GetUserByIdQuery() { Id = userId });
+            var response = await _userService.GetUserById(userId);
+
+            return Ok(response);
+        }
+
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(AttributeListDto))]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [SwaggerOperation("This method can be used by an authorized user.")]
+        public async Task<IActionResult> GetCurrentAttributes()
+        {
+            var userId = User.GetLoggedInUserId();
+
+            var response = await _userService.GetUserAttributesById(userId); ;
+
+            return Ok(response);
+        }
+
+        [HttpGet("{userId}")]
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(AttributeListDto))]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [SwaggerOperation("This method can only be used by the administrator.")]
+        [Authorize(Roles = "Admin")]
+        public async Task<IActionResult> GetAttributesNyId([Required] Guid userId)
+        {
+            var response = await _userService.GetUserById(userId);
+
+            return Ok(response);
+        }
+    }
+}
---
+++ Microservices/ClientsMicroservice/Data/ClaimsPrincipalExtensions.cs
@@ -0,0 +1,34 @@
+﻿#nullable disable
+using System.Security.Claims;
+
+namespace ClientsMicroservice.Data
+{
+    public static class ClaimsPrincipalExtensions
+    {
+        public static Guid GetLoggedInUserId(this ClaimsPrincipal principal)
+        {
+            if (principal == null)
+                throw new ArgumentNullException(nameof(principal));
+
+            var loggedInUserId = principal.FindFirst(ClaimTypes.NameIdentifier).Value;
+
+            return new Guid(loggedInUserId);
+        }
+
+        public static string GetLoggedInUserName(this ClaimsPrincipal principal)
+        {
+            if (principal == null)
+                throw new ArgumentNullException(nameof(principal));
+
+            return principal.FindFirst(ClaimTypes.Name).Value;
+        }
+
+        public static string GetLoggedInUserEmail(this ClaimsPrincipal principal)
+        {
+            if (principal == null)
+                throw new ArgumentNullException(nameof(principal));
+
+            return principal.FindFirst(ClaimTypes.Email).Value;
+        }
+    }
+}
