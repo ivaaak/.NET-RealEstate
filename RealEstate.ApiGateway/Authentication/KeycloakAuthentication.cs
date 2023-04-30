43b040fb65aab53ff7a6839118f3fb2fa4ea248c
Ivaylo Pavlov <ivaaakpavlov@gmail.com>
2023-04-30 13:07:45 +0300

    feat: keycloak authentication in the api gateway

---
+++ RealEstate.ApiGateway/Authentication/KeycloakAuthentication.cs
@@ -0,0 +1,19 @@
+﻿#nullable disable
+using Keycloak.AuthServices.Authentication;
+
+namespace RealEstate.ApiGateway.Authentication
+{
+    public static class KeycloakAuthentication
+    {
+        public static IServiceCollection AddKeycloakAuthenticationConfigured(this IServiceCollection services, IConfiguration configuration)
+        {
+            var authenticationOptions = configuration
+                .GetSection(KeycloakAuthenticationOptions.Section)
+                .Get<KeycloakAuthenticationOptions>();
+
+            services.AddKeycloakAuthentication(authenticationOptions);
+
+            return services;
+        }
+    }
+}
--- RealEstate.ApiGateway/Program.cs
+++ RealEstate.ApiGateway/Program.cs
@@ -3,6 +3,9 @@ using Ocelot.Middleware;
 using Ocelot.Cache.CacheManager;
 using RealEstate.Shared.Logging;
 using Serilog;
+using System.Configuration;
+using Keycloak.AuthServices.Authentication;
+using RealEstate.ApiGateway.Authentication;
 
 var builder = WebApplication.CreateBuilder(args);
 // Configure API Gateway
@@ -17,13 +20,15 @@ builder.Configuration.SetBasePath(builder.Environment.ContentRootPath)
 
 // Run the Gateway on port 9000
 builder.WebHost.UseUrls("http://*:9000");
-builder.Host.UseSerilog(SeriLogger.Configure);
+builder.Host.UseSerilog(SeriLogger.Configure); // Change to ApplicationLogger.ConfigureLogger ?
 
 builder.Services
     .AddOcelot(builder.Configuration)
     .AddCacheManager(settings => settings.WithDictionaryHandle());
 builder.Services.AddTransient<LoggingDelegatingHandler>();
 builder.Services.AddCors();
+
+builder.Services.AddKeycloakAuthenticationConfigured(builder.Configuration);
 
 
 var app = builder.Build();
@@ -32,10 +37,10 @@ await app.UseOcelot();
 app.UseCors();
 
 // App routing
-app.UseHttpsRedirection()
-    .UseStaticFiles()
-    .UseRouting()
-    .UseAuthentication()
-    .UseAuthorization();
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+app.UseRouting();
+app.UseAuthentication();
+app.UseAuthorization();
 
 app.Run();
