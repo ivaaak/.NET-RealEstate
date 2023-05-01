184d7ec87f31ad898d5c64af8ab2974bbde4c76f
Ivaylo Pavlov <ivaaakpavlov@gmail.com>
2023-05-01 12:43:33 +0300

    fix: logging improvements

---
+++ RealEstate.Shared/Logging/ApplicationLogger.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Serilog;
+using Serilog.Events;
+
+namespace RealEstate.Shared.Logging
+{
+    public static class ApplicationLogger
+    {
+        public static void ConfigureLogger(this ConfigureHostBuilder host)
+        {
+            Log.Logger = new LoggerConfiguration()
+                .MinimumLevel.Verbose()
+                .WriteTo.Console(
+                    outputTemplate: "[{Level:u4}] |{SourceContext,30}({EventId})| {Message:lj}{NewLine}{Exception}",
+                    restrictedToMinimumLevel: LogEventLevel.Debug)
+                .CreateBootstrapLogger();
+
+            host.UseSerilog();
+        }
+    }
+}
---
+++ RealEstate.Shared/Logging/ServiceCollectionExtensions.Logging.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Serilog;
+using Serilog.Events;
+
+namespace RealEstate.Shared.Logging
+{
+    public static partial class ServiceCollectionExtensions
+    {
+        public static WebApplicationBuilder AddSerilog(this WebApplicationBuilder builder)
+        {
+            Log.Logger = new LoggerConfiguration()
+                .MinimumLevel.Information()
+                .MinimumLevel.Override("Microsoft.AspNetCore.DataProtection.KeyManagement", LogEventLevel.Warning)
+                .MinimumLevel.Override("Microsoft.AspNetCore.Authorization", LogEventLevel.Verbose)
+                .MinimumLevel.Override("System.Net.Http", LogEventLevel.Debug)
+                .MinimumLevel.Override("Keycloak.AuthServices", LogEventLevel.Verbose)
+                //.WriteTo.Console(
+                //    "{SourceContext}{NewLine}{Timestamp:HH:mm:ss} [{Level:u4}] {Message:lj}{NewLine}{Exception}",
+                //    LogEventLevel.Verbose)
+                .CreateLogger();
+
+            builder.Host.UseSerilog();
+
+            return builder;
+        }
+    }
+}
