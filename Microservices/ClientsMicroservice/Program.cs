5ff087d8d3b428acc33d9c8498a6917b8ca9e575
Ivaylo Pavlov <ivaaakpavlov@gmail.com>
2023-05-09 17:45:42 +0300

    style: clients MS - data, startup fixes

--- Microservices/ClientsMicroservice/Program.cs
+++ Microservices/ClientsMicroservice/Program.cs
@@ -1,38 +1,29 @@
-using ClientsMicroservice.Data.Context;
-using ClientsMicroservice.Data.Repository;
-using ClientsMicroservice.Services;
-using RealEstate.Shared.Data.Repository;
+using ClientsMicroservice.Properties;
 using RealEstate.Shared.Logging;
 using RealEstate.Shared.ServiceExtensions;
 using Serilog;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.WebHost.UseUrls("http://*:9001");  // Run on port 9001
+// Run on port 9001
+builder.WebHost.UseUrls("http://*:9001");  
 builder.Host.UseSerilog(SeriLogger.Configure);
-
-builder.Services.AddTransient<IUserService, UserService>();
-builder.Services.AddTransient<IUserRepository, UserRepository>();
-builder.Services.AddDbContext<UsersDBContext>();
-builder.Services.AddDbContext<ClientsDBContext>();
-
-builder.Services.AddScoped<IClientsDbRepository, ClientsDbRepository>();
 
 builder.Services.AddControllers();
 builder.Services
     .AddEndpointsApiExplorer()
+    .AddRepositoriesAndContexts(builder.Configuration)
     .AddSwaggerWithConfig("Clients")
-    .AddRepositories()
     .AddRedisCacheWithConnectionString(builder)
     .AddMassTransitWithRabbitMQProvider(builder)
+    .AddMediatR(cfg => cfg.RegisterServicesFromAssembly(typeof(Program).Assembly))
     .AddHealthChecks();
-builder.Services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(typeof(Program).Assembly));
 
 var app = builder.Build();
-app.AddSwaggerDevelopmentDocs("Clients");
 
+app.AddSwaggerDevelopmentDocs("Clients");
 app.UseAuthentication().UseAuthorization();
 app.MapControllers();
-app.MapHealthCheckEndpoint();
+app.MapHealthChecks("/health");
 
 app.Run();
---
+++ Microservices/ClientsMicroservice/Properties/ClientsStartupExtensions.cs
@@ -0,0 +1,32 @@
+﻿using ClientsMicroservice.Data.Context;
+using ClientsMicroservice.Data.Repository;
+using ClientsMicroservice.Services;
+using Microsoft.EntityFrameworkCore;
+using RealEstate.Shared;
+using RealEstate.Shared.Data.Repository;
+
+namespace ClientsMicroservice.Properties
+{
+    public static class ClientsStartupExtensions
+    {
+        public static IServiceCollection AddRepositoriesAndContexts(this IServiceCollection services, IConfiguration configuration)
+        {
+            // Services
+            services.AddTransient<IUserService, UserService>();
+
+            // DbContexts using pooling for better performance
+            services.AddDbContextPool<UsersDBContext>(options => 
+                options.UseNpgsql(GlobalConnectionStrings.Clients_MicroDB_Connection));
+
+            services.AddDbContextPool<ClientsDBContext>(options =>
+                options.UseNpgsql(GlobalConnectionStrings.Clients_MicroDB_Connection));
+
+            // Repositories
+            services.AddScoped<IClientsDbRepository, ClientsDbRepository>();
+            services.AddScoped<IUserRepository, UserRepository>();
+            services.AddScoped<IRepository, Repository>(); //base repo implementation
+
+            return services;
+        }
+    }
+}
