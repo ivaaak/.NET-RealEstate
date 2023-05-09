c6735ea26dfc4e258fd14ce55fd61f7a052fe98d
Ivaylo Pavlov <ivaaakpavlov@gmail.com>
2023-05-09 17:46:45 +0300

    style: estates MS - data. startup fixes

--- Microservices/EstatesMicroservice/Program.cs
+++ Microservices/EstatesMicroservice/Program.cs
@@ -1,7 +1,4 @@
-using EstatesMicroservice.Services;
-using Microsoft.EntityFrameworkCore;
-using RealEstate.Shared.Data.Context;
-using RealEstate.Shared.Data.Repository;
+using EstatesMicroservice.Properties;
 using RealEstate.Shared.Logging;
 using RealEstate.Shared.ServiceExtensions;
 using Serilog;
@@ -13,24 +10,20 @@ builder.WebHost.UseUrls("http://*:9003");
 builder.Host.UseSerilog(SeriLogger.Configure);
 
 builder.Services.AddControllers();
-builder.Services.AddTransient<IEstateService, EstateService>();
-builder.Services.AddTransient<IEstatesDbRepository, EstatesDbRepository>();
-
-builder.Services.AddDbContext<EstatesDBContext>(options =>
-    options.UseNpgsql(builder.Configuration["DatabaseSettings:ConnectionString"]));
-
 builder.Services
     .AddEndpointsApiExplorer()
+    .AddRepositoriesAndContexts(builder.Configuration)
     .AddSwaggerWithConfig("Estates")
-    .AddRepositories()
     .AddRedisCacheWithConnectionString(builder)
     .AddMassTransitWithRabbitMQProvider(builder)
+    .AddMediatR(cfg => cfg.RegisterServicesFromAssembly(typeof(Program).Assembly))
     .AddHealthChecks();
-builder.Services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(typeof(Program).Assembly));
 
 var app = builder.Build();
+
 app.AddSwaggerDevelopmentDocs("Estates");
-app.UseHttpsRedirection().UseAuthorization();
+app.UseAuthentication().UseAuthorization();
 app.MapControllers();
-app.MapHealthCheckEndpoint();
+app.MapHealthChecks("/health");
+
 app.Run();
---
+++ Microservices/EstatesMicroservice/Properties/EstatesStartupExtentions.cs
@@ -0,0 +1,27 @@
+﻿using EstatesMicroservice.Services;
+using Microsoft.EntityFrameworkCore;
+using RealEstate.Shared;
+using RealEstate.Shared.Data.Context;
+using RealEstate.Shared.Data.Repository;
+
+namespace EstatesMicroservice.Properties
+{
+    public static class EstatesStartupExtentions
+    {
+        public static IServiceCollection AddRepositoriesAndContexts(this IServiceCollection services, IConfiguration configuration)
+        {
+            // Services
+            services.AddTransient<IEstateService, EstateService>();
+
+            // DbContexts using pooling for better performance
+            services.AddDbContextPool<EstatesDBContext>(options => 
+                options.UseNpgsql(GlobalConnectionStrings.Estates_MicroDB_Connection));
+
+            // Repositories
+            services.AddTransient<IEstatesDbRepository, EstatesDbRepository>();
+            services.AddScoped<IRepository, Repository>(); //base repo implementation
+
+            return services;
+        }
+    }
+}
