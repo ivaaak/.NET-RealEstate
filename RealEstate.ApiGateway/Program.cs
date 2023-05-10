473cb6fbc2f0ad53cdc899d45d4603d278f47aab
Ivaylo Pavlov <ivaaakpavlov@gmail.com>
2023-05-10 19:45:30 +0300

    feat: api gateway - health checks, swagger

--- RealEstate.ApiGateway/Program.cs
+++ RealEstate.ApiGateway/Program.cs
@@ -3,7 +3,9 @@ using Ocelot.DependencyInjection;
 using Ocelot.Middleware;
 using RealEstate.ApiGateway.Authentication;
 using RealEstate.ApiGateway.Authorization;
+using RealEstate.ApiGateway.Properties;
 using RealEstate.Shared.Logging;
+using RealEstate.Shared.ServiceExtensions;
 using Serilog;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -16,25 +18,26 @@ builder.Configuration.SetBasePath(builder.Environment.ContentRootPath)
 builder.WebHost.UseUrls("http://*:9000");
 builder.Host.UseSerilog(SeriLogger.Configure); // Change to ApplicationLogger.ConfigureLogger ?
 
-builder.Services
-    .AddOcelot(builder.Configuration)
-    .AddCacheManager(settings => settings.WithDictionaryHandle());
+builder.Services.AddOcelot(builder.Configuration).AddCacheManager(s => s.WithDictionaryHandle());
 builder.Services.AddTransient<LoggingDelegatingHandler>();
-builder.Services.AddCors();
-
+builder.Services.AddCors().AddEndpointsApiExplorer();
+builder.Services.AddHealthCheckServices();
+builder.Services.AddSwaggerWithConfig("API Gateway");
 builder.Services.AddKeycloakAuthenticationConfigured(builder.Configuration);
 builder.Services.AddKeycloakAuthorizationConfigured(builder.Configuration);
-
 
 var app = builder.Build();
 
 await app.UseOcelot();
 app.UseCors();
+app.AddSwaggerDevelopmentDocs("API Gateway");
 app.UseHttpsRedirection();
 app.UseApplicationSwagger(builder.Configuration);
 app.UseStaticFiles();
 app.UseRouting();
 app.UseAuthentication();
 app.UseAuthorization();
+app.MapControllers();
+app.MapHealthChecks("/health");
 
 app.Run();
