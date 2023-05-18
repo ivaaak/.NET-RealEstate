6894389a2751d29fa21e971e474b54689e7954a3
Ivaylo Pavlov <ivaaakpavlov@gmail.com>
2023-05-18 19:46:28 +0300

    testing: added functional + integration tests folder structure

---
+++ RealEstate.Test/IntegrationTests/PublicAPIIntegrationTests/ProgramTest.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NUnit.Framework;
+
+namespace RealEstate.Test.IntegrationTests.PublicAPIIntegrationTests
+{
+    [TestClass]
+    public class ProgramTest
+    {
+        private static WebApplicationFactory<Program> _application = new();
+
+        public static HttpClient NewClient
+        {
+            get
+            {
+                return _application.CreateClient();
+            }
+        }
+
+        [AssemblyInitialize]
+        public static void AssemblyInitialize(TestContext _)
+        {
+            _application = new WebApplicationFactory<Program>();
+
+        }
+    }
+}
---
+++ RealEstate.Test/IntegrationTests/Repositories/ClientsDbRepositoryTests/RepoTest.cs
@@ -0,0 +1,42 @@
+﻿using ClientsMicroservice.Data.Context;
+using Microsoft.EntityFrameworkCore;
+using NUnit.Framework;
+using RealEstate.Shared.Data.Repository;
+using RealEstate.Shared.Models.Entities.Clients;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace RealEstate.Test.IntegrationTests.Repositories.ClientsDbRepositoryTests
+{
+    public class GetById
+    {
+        private readonly ClientsDBContext _clientsContext;
+        private readonly ClientsDbRepository _clientsRepository;
+        private readonly ITestOutputHelper _output;
+        public GetById(ITestOutputHelper output)
+        {
+            _output = output;
+            var dbOptions = new DbContextOptionsBuilder<ClientsDBContext>()
+                .UseInMemoryDatabase(databaseName: "TestClient")
+                .Options;
+            _clientsContext = new ClientsDBContext(dbOptions);
+            _clientsRepository = new ClientsDbRepository(_clientsContext);
+        }
+
+        [Fact]
+        public async Task GetsExistingClient()
+        {
+            var existingClient = new Client()
+            {
+
+            };
+            _clientsContext.Clients.Add(existingClient);
+            _clientsContext.SaveChanges();
+            string clientId = existingClient.Client_Id;
+            _output.WriteLine($"clientId: {clientId}");
+
+            var clientFromRepo = await _clientsRepository.GetByIdAsync<Client>(clientId);
+            Assert.AreEqual("thing", clientFromRepo.Client_Id);
+        }
+    }
+}
