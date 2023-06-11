a1cf6e165db1dcf46c11df8bb34ca96451741273
Ivaylo Pavlov <ivaaakpavlov@gmail.com>
2023-06-11 12:33:29 +0300

    feat: projects - service, controller

---
+++ Microservices/ContractsMicroservice/Controllers/ProjectController.cs
@@ -0,0 +1,187 @@
+﻿using ContractsMicroservice.Services.Interfaces;
+using MassTransit;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using RealEstate.Shared.Models.Entities.Contracts;
+using RealEstate.Shared.Models.Entities.Misc;
+
+namespace ContractsMicroservice.Controllers
+{
+    [Authorize]
+    [ApiController]
+    [Route("api/[controller]/[action]")]
+    public class ProjectController : ControllerBase
+    {
+        private readonly IProjectService _documentService;
+
+        private readonly ILogger<ProjectController> _logger;
+
+        private readonly IPublishEndpoint _publishEndpoint;
+
+        public ProjectController(
+            IProjectService documentService,
+            ILogger<ProjectController> logger,
+            IPublishEndpoint publishEndpoint)
+        {
+            _documentService = documentService ?? throw new ArgumentNullException(nameof(documentService));
+            _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+            _publishEndpoint = publishEndpoint ?? throw new ArgumentNullException(nameof(publishEndpoint));
+        }
+
+
+        [HttpPost]
+        public IActionResult UploadProject(int userId, Project model)
+        {
+            try
+            {
+                _documentService.ValidateModel(model);
+                _documentService.UploadProject(userId, model);
+                return Ok();
+            }
+            catch (ArgumentException ex)
+            {
+                // Log the error
+                // Return a BadRequest response with a validation error message
+                return BadRequest(ex.Message);
+            }
+            catch (Exception ex)
+            {
+                // Log the error
+                // Return a 500 Internal Server Error response
+                return StatusCode(500, ex.Message);
+            }
+        }
+
+
+        [HttpGet]
+        public IActionResult GetProjectsList(int userId)
+        {
+            try
+            {
+                var documents = _documentService.GetProjectsList(userId);
+                return Ok(documents);
+            }
+            catch (Exception)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError);
+            }
+        }
+
+
+        [HttpGet]
+        [Route("/{userId}/{documentId}")]
+        public async Task<IActionResult> DownloadProject(int userId, int documentId)
+        {
+            // Check if the user has the document
+            if (!await _documentService.CheckIfUserHasProject(userId, documentId))
+            {
+                // Return a "not found" error if the user doesn't have the document
+                return NotFound();
+            }
+
+            try
+            {
+                // Download the document
+                await _documentService.DownloadProject(Response, userId, documentId);
+            }
+            catch (Exception ex)
+            {
+                // Return an "internal server error" if something goes wrong during the download
+                return StatusCode(StatusCodes.Status500InternalServerError, ex);
+            }
+
+            return Ok();
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> DeleteProject(int userId, int documentId)
+        {
+            try
+            {
+                // Check if the user has the document
+                bool hasProject = await _documentService.CheckIfUserHasProject(userId, documentId);
+                if (!hasProject)
+                {
+                    throw new Exception("The user does not have this document");
+                }
+
+                // Delete the document
+                // (Implementation details will depend on the specific storage system being used)
+                await _documentService.DeleteProject(userId, documentId);
+                return NoContent();
+            }
+            catch (Exception ex)
+            {
+                // Log the error
+                // (Implementation details will depend on the specific logging system being used)
+
+                // Return an error response to the client
+                return BadRequest(ex.Message);
+            }
+        }
+
+
+        [HttpGet]
+        public async Task<IActionResult> CheckIfUserHasProject(int userId, int documentId)
+        {
+            try
+            {
+                bool result = await _documentService.CheckIfUserHasProject(userId, documentId);
+                return Ok(result);
+            }
+            catch (Exception ex)
+            {
+                // Log the exception
+                return StatusCode(500, ex.Message);
+            }
+        }
+
+
+        [HttpPost]
+        public IActionResult ValidateProject(Project model)
+        {
+            try
+            {
+                _documentService.ValidateModel(model);
+                return Ok();
+            }
+            catch (ArgumentException ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+
+        [HttpGet]
+        public string GetContentType(string fileName)
+        {
+            try
+            {
+                var mimeType = GetContentTypeUtil();
+                if (string.IsNullOrEmpty(mimeType))
+                {
+                    throw new Exception($"Could not determine MIME type for file '{fileName}'");
+                }
+
+                return mimeType;
+            }
+            catch (Exception ex)
+            {
+                // Log the error
+                Console.WriteLine($"Error getting content type for file '{fileName}': {ex.Message}");
+                throw;
+            }
+        }
+
+        [HttpGet]
+        [AllowAnonymous]
+        // http://localhost:9002/api/document/gethealth
+        // http://localhost:9000/api/document/gethealth behind gateway
+        public string getHealth() => "Contracts Microservice up and running";
+
+        static string GetContentTypeUtil()
+        {
+            return System.Net.Mime.MediaTypeNames.Application.Octet;
+        }
+    }
+}
---
+++ Microservices/ContractsMicroservice/Services/ProjectService.cs
@@ -0,0 +1,134 @@
+﻿using ContractsMicroservice.Services.Interfaces;
+using RealEstate.Shared.Data.Repository;
+using RealEstate.Shared.Models.Entities.Contracts;
+
+namespace ContractsMicroservice.Services
+{
+    public class ProjectService : IProjectService
+    {
+        private readonly IRepository repo;
+
+        public ProjectService(IRepository _repo)
+        {
+            repo = _repo;
+        }
+
+
+        public void UploadProject(int userId, Project model)
+        {
+            ValidateModel(model);
+
+            if (userId.ToString() != model.Client_Id)
+            {
+                throw new ArgumentException("Invalid userId");
+            }
+
+            // Save the file to a secure location
+            repo.AddAsync(model);
+            repo.SaveChanges();
+        }
+
+
+        public IEnumerable<Project> GetProjectsList(int userId)
+        {
+            // Validate the userId
+            if (userId <= 0)
+            {
+                throw new ArgumentException("The userId must be a positive integer.");
+            }
+
+            // Retrieve the list of documents for the specified user
+            var documents = repo.All<Project>().Where(d => d.Id == userId.ToString()).ToList();
+
+            return documents;
+        }
+
+
+        public async Task DownloadProject(HttpResponse response, int userId, int documentId)
+        {
+            // Retrieve the specified document
+            var document = repo.GetByIdAsync<Project>(documentId).Result;
+
+            if (document == null)
+            {
+                throw new ArgumentException("Invalid documentId");
+            }
+
+            if (userId.ToString() != document.Id)
+            {
+                throw new ArgumentException("Invalid userId");
+            }
+
+            // Send the document to the client as a download
+            var memory = new MemoryStream();
+            //await memory.WriteAsync(document.Text_Content, 0, document.Text_Content.Length);
+            memory.Position = 0;
+            response.ContentType = GetContentType(document.Title);
+            response.Headers.Add("Content-Disposition", "attachment; filename=" + document.Title);
+
+            //await response.Body.WriteAsync(document.Title, 0, document.Text_Content.Length);
+        }
+
+
+        public async Task DeleteProject(int userId, int documentId)
+        {
+            // retrieve the document
+            var document = await repo.GetByIdAsync<Project>(documentId);
+
+            // check that the document belongs to the specified user
+            if (document.Client_Id != userId.ToString())
+            {
+                throw new ArgumentException("Invalid userId");
+            }
+
+            // delete the document
+            await repo.DeleteAsync<Project>(document);
+        }
+
+
+        public async Task<bool> CheckIfUserHasProject(int userId, int documentId)
+        {
+            // retrieve the document
+            var document = await repo.GetByIdAsync<Project>(documentId);
+
+            // check if the document belongs to the specified user
+            return document.Client_Id == userId.ToString();
+        }
+
+
+        public void ValidateModel(Project model)
+        {
+            if (model == null)
+            {
+                throw new ArgumentNullException(nameof(model));
+            }
+
+            if (string.IsNullOrEmpty(model.Title))
+            {
+                throw new ArgumentException("Title cannot be null or empty", nameof(model));
+            }
+        }
+
+
+        public string GetContentType(string fileName)
+        {
+            var extension = Path.GetExtension(fileName).ToLowerInvariant();
+            var contentType = "application/octet-stream";
+
+            if (extension == ".pdf")
+            {
+                contentType = "application/pdf";
+            }
+            else if (extension == ".docx")
+            {
+                contentType = "application/vnd.openxmlformats-officedocument.wordprocessingml.document";
+            }
+            else if (extension == ".xlsx")
+            {
+                contentType = "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet";
+            }
+
+            return contentType;
+        }
+    }
+}
