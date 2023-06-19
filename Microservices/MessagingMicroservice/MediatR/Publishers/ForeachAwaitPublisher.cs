b5645ad6620e8548e9bd75f9824d61cce51226c8
Ivaylo Pavlov <ivaaakpavlov@gmail.com>
2023-06-19 18:30:10 +0300

    feat: notification publishers

---
+++ Microservices/MessagingMicroservice/MediatR/Publishers/ForeachAwaitPublisher.cs
@@ -0,0 +1,20 @@
+﻿using MediatR;
+
+namespace MessagingMicroservice.MediatR.Publishers
+{
+    public class ForeachAwaitPublisher : INotificationPublisher
+    {
+        public async Task Publish(
+            IEnumerable<NotificationHandlerExecutor> handlerExecutors,
+            INotification notification,
+            CancellationToken cancellationToken)
+        {
+            foreach (var handler in handlerExecutors)
+            {
+                await handler
+                    .HandlerCallback(notification, cancellationToken)
+                    .ConfigureAwait(false);
+            }
+        }
+    }
+}
---
+++ Microservices/MessagingMicroservice/MediatR/Publishers/TaskWhenAllPublisher.cs
@@ -0,0 +1,21 @@
+﻿using MediatR;
+
+namespace MessagingMicroservice.MediatR.Publishers
+{
+    public class TaskWhenAllPublisher : INotificationPublisher
+    {
+        public Task Publish(
+            IEnumerable<NotificationHandlerExecutor> handlerExecutors,
+            INotification notification,
+            CancellationToken cancellationToken)
+        {
+            var tasks = handlerExecutors
+                .Select(handler => handler.HandlerCallback(
+                    notification,
+                    cancellationToken))
+                .ToArray();
+
+            return Task.WhenAll(tasks);
+        }
+    }
+}
