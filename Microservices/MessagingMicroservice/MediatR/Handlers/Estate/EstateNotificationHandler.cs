808cfd6394fb88cec6a709492ad2fd66ead1cb11
Ivaylo Pavlov <ivaaakpavlov@gmail.com>
2023-06-20 20:25:51 +0300

    feat: notification handler example, extended estate notification object

---
+++ Microservices/MessagingMicroservice/MediatR/Handlers/Estate/EstateNotificationHandler.cs
@@ -0,0 +1,37 @@
+﻿using global::MediatR;
+using MassTransit;
+using MessagingMicroservice.MediatR.Notifications.Estates;
+
+namespace MessagingMicroservice.MediatR.Handlers.Estate
+{
+    public class EstateNotificationHandler : INotificationHandler<EstateNotification>
+    {
+        private readonly IPublishEndpoint _publishEndpoint;
+
+        private readonly ILogger<EstateNotificationHandler> _logger;
+
+        public Task Handle(EstateNotification notification, CancellationToken cancellationToken)
+        {
+            // Handle the estate notification here
+            // You can perform any necessary processing or trigger other actions based on the notification
+            notification.HasBeenPublished = true;
+            notification.DateUpdated = DateTime.UtcNow;
+
+            // Go outside of the Messages Microservice
+            // Publish the notification to the RabbitMQ Message Bus/Queue
+            _publishEndpoint.Publish(notification);
+            _logger.LogInformation($"Published notification - {notification}");
+
+            // For example, you can log the estate details
+            Console.WriteLine($"Received estate notification with ID: {notification.Id}");
+            Console.WriteLine($"DateCreated: {notification.DateCreated}");
+            Console.WriteLine($"Estate Details: {notification.Estate}");
+            Console.WriteLine($"HasBeenPublished: {notification.HasBeenPublished}");
+            Console.WriteLine($"The notification has the following text: {notification.NotificationMessage}");
+
+            // Or you can send an email, update a database, etc.
+
+            return Task.CompletedTask;
+        }
+    }
+}
--- Microservices/MessagingMicroservice/MediatR/Notifications/Estates/EstateNotification .cs
+++ Microservices/MessagingMicroservice/MediatR/Notifications/Estates/EstateNotification .cs
@@ -7,5 +7,17 @@ namespace MessagingMicroservice.MediatR.Notifications.Estates
     {
         // MediatRNotificationBase - Id, DateCreated, DateUpdated, HasBeenSent
         public Estate? Estate { get; set; }
+
+        public Category? Category { get; set; }
+
+        public City? City { get; set; }
+
+        public Country? Country { get; set; }
+
+        public Estate_Status? Estate_Status { get; set; }
+
+        public Estate_Type? Estate_Type { get; set; }
+
+        public In_Charge? In_Charge { get; set; }
     }
 }
