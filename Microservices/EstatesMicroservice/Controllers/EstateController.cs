281b225b657ff05fd38d008fd58548d12aa88d49
Ivaylo Pavlov <ivaaakpavlov@gmail.com>
2023-09-09 14:30:01 +0300

    refactor/ Estate controller - types, documentation improvements

--- Microservices/EstatesMicroservice/Controllers/EstateController.cs
+++ Microservices/EstatesMicroservice/Controllers/EstateController.cs
@@ -5,81 +5,82 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using RealEstate.Shared.Models.DTOs.Estates;
 using RealEstate.Shared.Models.Entities.Estates;
+using RealEstate.Shared.Models.QueryObjects;
 using System.Net;
 
 namespace EstatesMicroservice.Controllers
 {
+    /// <summary>
+    /// Controller for managing estates.
+    /// </summary>
     [Authorize]
     [ApiController]
     [Consumes("application/json")]
     [Produces("application/json")]
-    [Route("api/[controller]/[action]")]  // /api/estate
+    [Route("api/estate")]
     public class EstateController : ControllerBase
     {
         private readonly IEstateService _estateService;
-
-        private readonly IMediator? _mediator;
-
+        //private readonly IEstateQueryService _estateQueryService;
+        private readonly IMediator _mediator;
         private readonly ILogger<EstateController> _logger;
-
         private readonly IPublishEndpoint _publishEndpoint;
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="EstateController"/> class.
+        /// </summary>
         public EstateController(
             IEstateService estateService,
+            //IEstateQueryService estateQueryService,
             IMediator mediator,
             ILogger<EstateController> logger,
             IPublishEndpoint publishEndpoint)
         {
             _estateService = estateService ?? throw new ArgumentNullException(nameof(estateService));
+            //_estateQueryService = estateQueryService ?? throw new ArgumentNullException(nameof(estateQueryService));
             _mediator = mediator ?? throw new ArgumentNullException(nameof(mediator));
             _logger = logger ?? throw new ArgumentNullException(nameof(logger));
             _publishEndpoint = publishEndpoint ?? throw new ArgumentNullException(nameof(publishEndpoint));
         }
-        
 
-        // CREATE: api/estate/create  //object
         /// <summary>
-        /// Creates an estate in the database if it doesnt already exist.
+        /// Creates an estate in the database if it doesn't already exist.
         /// </summary>
-        /// <param name="estate"> Estate Object </param>
+        /// <param name="estate">The estate object to create.</param>
         /// <remarks>
         /// Sample request:
         ///
-        ///     Post /api/estate/create 
+        ///     POST /api/estate/create
         ///
         /// </remarks>
-        /// <returns> Ok </returns>
-        [HttpPost]
+        [HttpPost("create")]
         [ProducesResponseType((int)HttpStatusCode.NotFound)]
-        [ProducesResponseType(typeof(ActionResult), (int)HttpStatusCode.OK)]
+        [ProducesResponseType(typeof(Estate), (int)HttpStatusCode.OK)]
         public ActionResult Create(Estate estate)
         {
             var result = _estateService.Create(estate);
 
             if (result == null)
             {
-                _logger.LogError($"Estate {estate} not created.");
+                _logger.LogError($"Estate {estate.Estate_Name} not created.");
                 return NotFound();
             }
 
             return Ok(result);
         }
 
-
-        // GET: api/estate/getall
         /// <summary>
         /// Gets all estates from the database.
         /// </summary>
         /// <remarks>
         /// Sample request:
         ///
-        ///     Post /api/estate/getall
+        ///     GET /api/estate/getall
         ///
         /// </remarks>
-        /// <returns> Ok </returns>
-        [HttpGet]
+        [HttpGet("getall")]
         [ProducesResponseType((int)HttpStatusCode.NotFound)]
-        [ProducesResponseType(typeof(ActionResult<IEnumerable<EstateDTO>>), (int)HttpStatusCode.OK)]
+        [ProducesResponseType(typeof(IEnumerable<EstateDTO>), (int)HttpStatusCode.OK)]
         public async Task<ActionResult<IEnumerable<EstateDTO>>> GetAll()
         {
             var result = await _estateService.GetEstates();
@@ -93,193 +94,175 @@ namespace EstatesMicroservice.Controllers
             return Ok(result);
         }
 
-
-        // GET: api/estate/getbyid/5
         /// <summary>
-        /// Gets an estate with the id if it exists.
+        /// Gets an estate with the specified ID if it exists.
         /// </summary>
-        /// <param name="id"> id - int </param>
+        /// <param name="id">The ID of the estate.</param>
         /// <remarks>
         /// Sample request:
         ///
-        ///     Post /api/estate/getbyid/5
+        ///     GET /api/estate/getbyid/5
         ///
         /// </remarks>
-        /// <returns> Estate Object </returns>
-        [HttpGet]
-        [Route("GetById/{id}")]
+        [HttpGet("getbyid/{id}")]
         [ProducesResponseType((int)HttpStatusCode.NotFound)]
-        [ProducesResponseType(typeof(ActionResult<EstateDTO>), (int)HttpStatusCode.OK)]
+        [ProducesResponseType(typeof(EstateDTO), (int)HttpStatusCode.OK)]
         public async Task<ActionResult<EstateDTO>> GetById(int id)
         {
             var result = await _estateService.GetEstateById(id);
 
-            //automapper for the result?
-
             if (result == null)
             {
-                _logger.LogError($"Estate with id: {id} not found.");
+                _logger.LogError($"Estate with ID: {id} not found.");
                 return NotFound();
             }
 
             return Ok(result);
         }
 
-
-        // PUT: api/estate/update/5
         /// <summary>
         /// Updates an estate's values/properties.
         /// </summary>
-        /// <param name="id"> id - int </param>
-        /// <param name="estateObject"> estate - object </param>
+        /// <param name="id">The ID of the estate to update.</param>
+        /// <param name="estateObject">The updated estate object.</param>
         /// <remarks>
         /// Sample request:
         ///
-        ///     Put /api/estate/update/5
+        ///     PUT /api/estate/update/5
         ///
         /// </remarks>
-        /// <returns> Ok </returns>
-        [HttpPut]
-        [Route("Update/{id}")]
+        [HttpPut("update/{id}")]
         [ProducesResponseType((int)HttpStatusCode.NotFound)]
-        [ProducesResponseType(typeof(ActionResult), (int)HttpStatusCode.OK)]
+        [ProducesResponseType(typeof(bool), (int)HttpStatusCode.OK)]
         public async Task<IActionResult> Update(int id, EstateDTO estateObject)
         {
             var result = await _estateService.UpdateEstate(id, estateObject);
 
             if (!result)
             {
-                _logger.LogError($"Estate with id: {id} not updated.");
+                _logger.LogError($"Estate with ID: {id} not updated.");
                 return NotFound();
             }
 
             return Ok(result);
         }
 
-
-        // DELETE: api/estate/delete/5
         /// <summary>
         /// Deletes an estate.
         /// </summary>
-        /// <param name="id"> id - int </param>
+        /// <param name="id">The ID of the estate to delete.</param>
         /// <remarks>
         /// Sample request:
         ///
-        ///     Put /api/estate/delete/5
+        ///     DELETE /api/estate/delete/5
         ///
         /// </remarks>
-        /// <returns> Ok - result </returns>
-        [HttpDelete]
-        [Route("Delete/{id}")]
+        [HttpDelete("delete/{id}")]
         [ProducesResponseType((int)HttpStatusCode.NotFound)]
-        [ProducesResponseType(typeof(ActionResult), (int)HttpStatusCode.OK)]
+        [ProducesResponseType(typeof(bool), (int)HttpStatusCode.OK)]
         public async Task<ActionResult> Delete(int id)
         {
             var result = await _estateService.SoftDeleteEstate(id);
 
             if (!result)
             {
-                _logger.LogError($"Estate with id: {id} not deleted.");
+                _logger.LogError($"Estate with ID: {id} not deleted.");
                 return NotFound();
             }
 
             return Ok(result);
         }
 
-
-        // HARD DELETE: api/estate/darddelete/5
         /// <summary>
-        /// Deletes an estate.
+        /// Deletes an estate permanently.
         /// </summary>
-        /// <param name="id"> id - int </param>
+        /// <param name="id">The ID of the estate to hard delete.</param>
         /// <remarks>
         /// Sample request:
         ///
-        ///     Put /api/estate/harddelete/5
+        ///     DELETE /api/estate/harddelete/5
         ///
         /// </remarks>
-        /// <returns> Ok - result </returns>
-        [HttpDelete]
-        [Route("HardDelete/{id}")]
+        [HttpDelete("harddelete/{id}")]
         [ProducesResponseType((int)HttpStatusCode.NotFound)]
-        [ProducesResponseType(typeof(ActionResult), (int)HttpStatusCode.OK)]
+        [ProducesResponseType(typeof(bool), (int)HttpStatusCode.OK)]
         public async Task<ActionResult> HardDelete(int id)
         {
             var result = await _estateService.HardDeleteEstate(id);
 
             if (!result)
             {
-                _logger.LogError($"Estate with id: {id} not hard deleted.");
+                _logger.LogError($"Estate with ID: {id} not hard deleted.");
                 return NotFound();
             }
 
             return Ok(result);
         }
 
-        // EXISTS: api/estate/exists/5
         /// <summary>
-        /// Returns a boolean - if the estate with the given id exists.
+        /// Checks if an estate with the given ID exists.
         /// </summary>
-        /// <param name="id"> id - int </param>
+        /// <param name="id">The ID of the estate to check.</param>
         /// <remarks>
         /// Sample request:
         ///
-        ///     Put /api/estate/exists/5
+        ///     GET /api/estate/exists/5
         ///
         /// </remarks>
-        /// <returns> exists - boolean </returns>
-        [HttpGet]
-        [Route("Exists/{id}")]
+        [HttpGet("exists/{id}")]
         [ProducesResponseType((int)HttpStatusCode.NotFound)]
-        [ProducesResponseType(typeof(ActionResult<bool>), (int)HttpStatusCode.OK)]
+        [ProducesResponseType(typeof(bool), (int)HttpStatusCode.OK)]
         public ActionResult<bool> Exists(int id)
         {
             var exists = _estateService.GetEstateById(id).Result != null;
 
             if (!exists)
             {
-                _logger.LogError($"Estate with id: {id} not found.");
+                _logger.LogError($"Estate with ID: {id} not found.");
                 return NotFound();
             }
 
             return Ok(exists);
         }
 
-
-        // SEARCH: api/estate/search/2-bedroom
         /// <summary>
         /// Returns a collection of estates which contain the search keyword.
         /// </summary>
-        /// <param name="searchTerm"> searchTerm - keyword (string) </param>
+        /// <param name="searchTerm">The search keyword (string).</param>
         /// <remarks>
         /// Sample request:
         ///
-        ///     Put /api/estate/search/{searchTerm}
+        ///     GET /api/estate/search/2-bedroom
         ///
         /// </remarks>
-        /// <returns> collection of estate objects </returns>
-        [HttpGet]
-        [Route("Search/{searchTerm}")]
-        [ProducesResponseType((int)HttpStatusCode.NotFound)]
+        [HttpGet("search/{searchTerm}")]
         [ProducesResponseType(typeof(IEnumerable<EstateDTO>), (int)HttpStatusCode.OK)]
-        public ActionResult<IEnumerable<EstateDTO>> Search(string searchTerm)
+        public async Task<ActionResult<IEnumerable<EstateDTO>>> Search(string searchTerm)
         {
-            var result = _estateService.SearchEstates(searchTerm).Result;
+            var result = await _estateService.SearchEstates(searchTerm);
 
-            if (result == null)
+            if (!result.Any())
             {
-                _logger.LogError($"Estate with searchTerm: {searchTerm} not found.");
-                return NotFound();
+                _logger.LogWarning($"No estates found for search term: {searchTerm}");
             }
 
             return Ok(result);
         }
 
 
-        [HttpGet]
+        /// <summary>
+        /// Returns a health status message for the microservice.
+        /// </summary>
+        /// <remarks>
+        /// Sample request:
+        ///
+        ///     GET /api/estate/health
+        ///
+        /// </remarks>
+        /// http://localhost:9003/api/estate
+        /// http://localhost:9000/api/estate behind gateway
+        [HttpGet("health")]
         [AllowAnonymous]
-        //http://localhost:9003/api/estate
-        //http://localhost:9000/api/estate behind gateway
-        public string getHealth() => "Estates Microservice up and running";
+        public string GetHealth() => "Estates Microservice up and running";
     }
 }
