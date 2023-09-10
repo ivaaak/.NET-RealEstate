69d5f1eba5b31d102a8b95e3f0928c81c47fcb4e
Ivaylo Pavlov <ivaaakpavlov@gmail.com>
2023-09-10 13:24:28 +0300

    feat: Filtration, Sorting, Pagination improvements

--- Microservices/EstatesMicroservice/Services/EstateQueryService.cs
+++ Microservices/EstatesMicroservice/Services/EstateQueryService.cs
@@ -1,11 +1,11 @@
 ﻿using AutoMapper;
 using EstatesMicroservice.Services.Interfaces;
-using MediatR;
 using Microsoft.EntityFrameworkCore;
 using RealEstate.Shared.Data.Repository;
 using RealEstate.Shared.Models.DTOs.Estates;
 using RealEstate.Shared.Models.Entities.Estates;
 using RealEstate.Shared.Models.QueryObjects;
+using System.Linq.Expressions;
 
 namespace EstatesMicroservice.Services
 {
@@ -15,16 +15,12 @@ namespace EstatesMicroservice.Services
 
         private readonly IMapper mapper;
 
-        private readonly IMediator mediator;
-
         public EstateQueryService(
             IRepository repository,
-            IMapper mapper,
-            IMediator mediator)
+            IMapper mapper)
         {
             this.repository = repository ?? throw new ArgumentNullException(nameof(repository));
             this.mapper = mapper ?? throw new ArgumentNullException(nameof(mapper));
-            this.mediator = mediator ?? throw new ArgumentNullException(nameof(mediator));
         }
 
         // Filter / Sort / Paginate
@@ -32,24 +28,42 @@ namespace EstatesMicroservice.Services
         {
             var query = repository.All<Estate>();
 
-            // Apply filters based on the criteria -> Search Term
-            // TODO make this separate filters for multiple fields 
-            // only 1 searchterm supported atm
-            if (!string.IsNullOrEmpty(criteria.SearchTerm))
-            {
-                query = query.Where(e =>
-                    e.Estate_Name.Contains(criteria.SearchTerm) ||
-                    e.Estate_Description.Contains(criteria.SearchTerm) ||
-                    e.Estate_Type.Type_Name.Contains(criteria.SearchTerm) ||
-                    e.Listing.Name.Contains(criteria.SearchTerm));
-            }
+            // Filtration
+            var filteredQuery = ApplyFiltering(criteria, criteria.SearchTerm, query);
+            if (filteredQuery != null) { query = filteredQuery; }
 
+            // Sorting
+            var sortedQuery = ApplySorting(criteria, query);
+            if (sortedQuery != null) { query = sortedQuery; }
+
+            // Calculate total count before applying pagination
+            var totalCount = await query.CountAsync();
+
+            // Pagination
+            var pagedQuery = ApplyPagination(criteria, query);
+            if (pagedQuery != null) { query = pagedQuery; }
+
+            // Map Estates to EstateDTOs
+            var estateDTOs = mapper.Map<IEnumerable<EstateDTO>>(query);
+
+            return new PagedResult<EstateDTO>
+            {
+                Items = estateDTOs,
+                TotalItems = totalCount,
+                PageNumber = criteria.PageNumber,
+                PageSize = criteria.PageSize
+            };
+        }
+
+
+        // Sorting
+        private IQueryable<Estate> ApplySorting(SearchCriteriaObject criteria, IQueryable<Estate>? query)
+        {
             // Apply sorting
-            // Apply sorting
-            if (!string.IsNullOrEmpty(criteria.SortBy))
+            if (!string.IsNullOrEmpty(criteria.SortByColumn) && query != null)
             {
                 // Try to find a field in the Estate Entity == SortBy in the SearchCriteriaObject
-                var sortingProperty = typeof(Estate).GetProperty(criteria.SortBy);
+                var sortingProperty = typeof(Estate).GetProperty(criteria.SortByColumn);
 
                 if (sortingProperty != null) // Sorting column found -> sort by its key
                 {
@@ -68,25 +82,69 @@ namespace EstatesMicroservice.Services
                 }
             }
 
-            // Calculate total count before applying pagination
-            var totalCount = await query.CountAsync();
+            return query;
+        }
 
-            // Apply pagination
-            var pagedQuery = query
-                .Skip((criteria.PageNumber - 1) * criteria.PageSize)
-                .Take(criteria.PageSize);
 
-            var estates = await pagedQuery.ToListAsync();
-
-            var estateDTOs = mapper.Map<IEnumerable<EstateDTO>>(estates);
-
-            return new PagedResult<EstateDTO>
+        // Filtration
+        private IQueryable<Estate> ApplyFiltering(SearchCriteriaObject criteria, string? searchTerm, IQueryable<Estate>? query)
+        {
+            if (query == null)
             {
-                Items = estateDTOs,
-                TotalItems = totalCount,
-                PageNumber = criteria.PageNumber,
-                PageSize = criteria.PageSize
-            };
+                throw new ArgumentNullException(nameof(query));
+            }
+
+            if (!string.IsNullOrEmpty(criteria.FilterByColumn) && !string.IsNullOrEmpty(searchTerm))
+            {
+                var filteringProperty = typeof(Estate).GetProperty(criteria.FilterByColumn);
+
+                if (filteringProperty != null)
+                {
+                    var parameter = Expression.Parameter(typeof(Estate), "e");
+                    var propertyAccess = Expression.Property(parameter, filteringProperty);
+                    var searchValue = Expression.Constant(searchTerm, typeof(string));
+                    var containsMethod = typeof(string).GetMethod("Contains", new[] { typeof(string) });
+                    var toLowerMethod = typeof(string).GetMethod("ToLower", System.Type.EmptyTypes);
+
+                    // Make the search case-insensitive
+                    var propertyToLower = Expression.Call(propertyAccess, toLowerMethod);
+                    var searchValueToLower = Expression.Call(searchValue, toLowerMethod);
+
+                    var containsExpression = Expression.Call(propertyToLower, containsMethod, searchValueToLower);
+                    var lambda = Expression.Lambda<Func<Estate, bool>>(containsExpression, parameter);
+                    return query.Where(lambda);
+                }
+                else
+                {
+                    throw new ArgumentException($"Sorting column '{criteria.FilterByColumn}' does not exist.");
+                }
+            }
+
+            return query;
+        }
+
+
+        // Pagination
+        private IQueryable<Estate> ApplyPagination(SearchCriteriaObject criteria, IQueryable<Estate>? query)
+        {
+            if (query == null)
+            {
+                throw new ArgumentNullException(nameof(query));
+            }
+
+            if (criteria.PageNumber >= 0 && criteria.PageSize > 0) {
+                var pagedQuery = query
+                    .Skip((criteria.PageNumber - 1) * criteria.PageSize)
+                    .Take(criteria.PageSize);
+
+                query = (IQueryable<Estate>)pagedQuery.ToList();
+            } 
+            else 
+            {
+                throw new ArgumentException(nameof(criteria));
+            }
+
+            return query;
         }
     }
 }
--- RealEstate.Shared/Models/QueryObjects/SearchCriteriaObject.cs
+++ RealEstate.Shared/Models/QueryObjects/SearchCriteriaObject.cs
@@ -2,12 +2,17 @@
 {
     public class SearchCriteriaObject
     {
+        // Filtration
         public string? SearchTerm { get; set; }
 
-        public string? SortBy { get; set; }
+        public string? FilterByColumn { get; set; }
+
+        // Sorting
+        public string? SortByColumn { get; set; }
 
         public bool SortDescending { get; set; }
 
+        // Pagination
         public int PageNumber { get; set; }
 
         public int PageSize { get; set; }
